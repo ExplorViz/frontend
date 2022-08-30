303227c0628dd60baf109e4419082da5169f2520
Alexander Krause <akr@informatik.uni-kiel.de>
2022-08-30 15:50:50 +0200

    Embroider Project

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const { Webpack } = require('@embroider/webpack');
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
@@ -20,5 +21,5 @@ module.exports = function (defaults) {
   // please specify an object with the list of modules as keys
   // along with the exports of each module as its value.
 
-  return app.toTree();
+  return require('@embroider/compat').compatBuild(app, Webpack);
 };
