39030b4dc9c3456d7ce0f941052849c1f37c8429
Alexander Krause <akr@informatik.uni-kiel.de>
2022-09-13 14:09:46 +0200

    fix router

--- app/router.js
+++ app/router.js
@@ -1,15 +1,27 @@
 import EmberRouter from '@ember/routing/router';
-import config from 'explorviz-frontend/config/environment';
+import config from './config/environment';
 
-export default class Router extends EmberRouter {
-  location = config.locationType;
-  rootURL = config.rootURL;
-}
+/**
+ * Ember router for mapping "route" and respective "template".
+ *
+ * @class Router
+ * @extends Ember.Router
+ *
+ * @module ember
+ */
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL,
+  configurationRouteExtensions: null,
+});
 
-Router.map(() => {
+// eslint-disable-next-line func-names, array-callback-return
+Router.map(function () {
   this.route('badroute', { path: '/*path' });
   this.route('login');
   this.route('callback');
   this.route('visualization');
   this.route('landscapes');
 });
+
+export default Router;
