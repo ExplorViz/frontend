f4241d486bbad30d7374fd79fcfff57ec60998a2
Alexander Krause <akr@informatik.uni-kiel.de>
2023-08-17 15:59:33 +0200

    fix linting

--- app/modifiers/landscape-data-watcher.ts
+++ app/modifiers/landscape-data-watcher.ts
@@ -99,9 +99,10 @@ export default class LandscapeDataWatcherModifier extends Modifier<Args> {
         );
 
         // create or update applicationObject3D
-        const app = await this.applicationRenderer.addApplicationTask.perform(
-          applicationData
-        );
+        const app =
+          await this.applicationRenderer.addApplicationTask.perform(
+            applicationData
+          );
 
         // fix previously existing nodes to position (if present) and calculate collision size
         const graphNode = graphNodes.findBy(
