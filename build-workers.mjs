c970d41dd4f63aafd58497339189fa73eb19a9d6
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2023-08-24 20:24:55 +0200

    Convert flat-data-worker to TS

--- build-workers.mjs
+++ build-workers.mjs
@@ -5,7 +5,8 @@ const args = new Set(process.argv.slice(2));
 const ctx = await esbuild.context({
     entryPoints: [
         'workers/city-layouter.ts',
-        'workers/metrics-worker.ts'
+        'workers/metrics-worker.ts',
+        'workers/flat-data-worker.ts'
     ],
     outdir: 'public/assets/web-workers',
     bundle: true,
