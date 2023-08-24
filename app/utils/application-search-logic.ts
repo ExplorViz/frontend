26fae3d79f0237856c9a751c6fe68650f99931e0
Alexander Krause <akr@informatik.uni-kiel.de>
2023-08-24 19:33:35 +0200

    outsourcing search-logic

---
+++ app/utils/application-search-logic.ts
@@ -0,0 +1,75 @@
+import { setOwner } from '@ember/application';
+import { inject as service } from '@ember/service';
+import ApplicationRenderer from 'explorviz-frontend/services/application-renderer';
+import ApplicationRepository from 'explorviz-frontend/services/repos/application-repository';
+import ClazzMesh from 'explorviz-frontend/view-objects/3d/application/clazz-mesh';
+import {
+  getAllAncestorComponents,
+  openComponentsByList,
+} from 'explorviz-frontend/utils/application-rendering/entity-manipulation';
+
+/**
+ * @class ApplicationSearchLogic
+ *
+ * @module explorviz
+ */
+export default class ApplicationSearchLogic {
+  @service('repos/application-repository')
+  applicationRepo!: ApplicationRepository;
+
+  @service
+  applicationRenderer!: ApplicationRenderer;
+
+  constructor(owner: any) {
+    // https://stackoverflow.com/questions/65010591/emberjs-injecting-owner-to-native-class-from-component
+    setOwner(this, owner);
+  }
+
+  getPossibleEntityNames(name: string) {
+    const searchString = name.toLowerCase();
+
+    let allEntities: Map<string, any> = new Map();
+
+    const applications = this.applicationRepo.getAll();
+
+    for (const application of applications) {
+      allEntities = new Map([...allEntities, ...application.flatData]);
+    }
+
+    const returnValue: any[] = [];
+
+    const entriesArray = Array.from(allEntities.entries());
+
+    for (let i = 0; i < entriesArray.length; i++) {
+      const [, value] = entriesArray[i];
+
+      if (returnValue.length === 10) {
+        break;
+      }
+
+      if (value.fqn.toLowerCase().includes(searchString)) {
+        returnValue.push(value);
+      }
+    }
+
+    return returnValue;
+  }
+
+  highlightEntityMeshByModelId(modelId: string, applicationModelId: string) {
+    const clazzModel = (
+      this.applicationRenderer.getBoxMeshByModelId(modelId) as ClazzMesh
+    ).dataModel;
+
+    const applicationObject3D =
+      this.applicationRenderer.getApplicationById(applicationModelId);
+
+    if (applicationObject3D) {
+      openComponentsByList(
+        getAllAncestorComponents(clazzModel),
+        applicationObject3D
+      );
+
+      this.applicationRenderer.highlightModel(clazzModel, applicationModelId);
+    }
+  }
+}
