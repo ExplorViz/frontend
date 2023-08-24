f7c00354cc056154c162e523ccc60080e9a6ef94
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2023-08-24 19:54:40 +0200

    Convert metrics-worker.ts to TS
    Reuses code from city-layouter which has been moved to workers/utils.ts

---
+++ workers/worker-types.ts
@@ -0,0 +1,46 @@
+export type LayoutSegment = {
+  parent: null | LayoutSegment,
+  lowerChild: null | LayoutSegment,
+  upperRightChild: null | LayoutSegment,
+  startX: number,
+  startZ: number,
+  width: number,
+  height: number,
+  used: boolean
+}
+
+export type ReducedClass = {
+  id: string,
+  name: string,
+  instanceCount: number,
+  methods: ReducedMethod[]
+}
+
+export type ReducedMethod = {
+  name: string;
+  hashCode: string // TODO tiwe
+}
+
+export type ReducedComponent = {
+  id: string,
+  name: string,
+  classes: ReducedClass[],
+  subPackages: ReducedComponent[] // TODO tiwe
+}
+
+export type ReducedApplication = {
+  id: string,
+  packages: ReducedComponent[]
+}
+
+export type Trace = {
+  spanList: Span[]
+}
+
+export type Span = {
+  parentSpanId: '' | string,
+  hashCode: string,
+  spanId: string,
+  startTime: number,
+  endTime: number
+};
