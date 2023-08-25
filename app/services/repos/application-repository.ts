41205e575d3379e67cda331c82ecb8aef49bfe38
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2023-08-25 17:00:44 +0200

    Simplify ApplicationRepository

--- app/services/repos/application-repository.ts
+++ app/services/repos/application-repository.ts
@@ -1,19 +1,8 @@
 import Service from '@ember/service';
-import { tracked } from '@glimmer/tracking';
 import ApplicationData from 'explorviz-frontend/utils/application-data';
-import { ApplicationCommunication } from 'explorviz-frontend/utils/landscape-rendering/application-communication-computer';
 
-export default class ApplicationRepository extends Service.extend({
-  // anything which *must* be merged to prototype here
-}) {
-  @tracked
-  applications: Map<string, ApplicationData> = new Map<
-    string,
-    ApplicationData
-  >();
-
-  @tracked
-  communications: ApplicationCommunication[] = [];
+export default class ApplicationRepository extends Service {
+  applications: Map<string, ApplicationData> = new Map();
 
   getById(applicationId: string) {
     return this.applications.get(applicationId);
