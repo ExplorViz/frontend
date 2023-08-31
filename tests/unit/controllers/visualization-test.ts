d9ecf1a2ff39aca5fbb8c753f7e23902a40deeeb
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2023-09-01 00:15:19 +0200

    Attempt to fix test

--- tests/unit/controllers/visualization-test.ts
+++ tests/unit/controllers/visualization-test.ts
@@ -1,12 +1,15 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+//import type VisualizationController from 'explorviz-frontend/controllers/visualization';
+import type VisualizationController from '../../../app/controllers/visualization';
 
 module('Unit | Controller', function (hooks) {
   setupTest(hooks);
 
   // Replace this with your real tests.
   test('it exists', function (assert) {
-    const controller = this.owner.lookup('controller:visualization');
+    const controller = this.owner.lookup('controller:visualization') as VisualizationController;
+    controller.setupListeners();
     assert.ok(controller);
   });
 });
