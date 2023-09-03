db261ea46294667a218ae05c76f1371447285e5d
Tim Weißenfels <10491533+tim-we@users.noreply.github.com>
2023-09-04 00:10:06 +0200

    Extract duplicated code for IDE communication

--- app/ide/shared.ts
+++ app/ide/shared.ts
@@ -1,4 +1,15 @@
+import type {
+  Application,
+  Class,
+  Package,
+} from 'explorviz-frontend/utils/landscape-schemes/structure-data';
 import type ApplicationObject3D from 'explorviz-frontend/view-objects/3d/application/application-object-3d';
+
+import ClazzCommunicationMesh from 'explorviz-frontend/view-objects/3d/application/clazz-communication-mesh';
+import ClazzMesh from 'explorviz-frontend/view-objects/3d/application/clazz-mesh';
+import CommunicationArrowMesh from 'explorviz-frontend/view-objects/3d/application/communication-arrow-mesh';
+import ComponentMesh from 'explorviz-frontend/view-objects/3d/application/component-mesh';
+import FoundationMesh from 'explorviz-frontend/view-objects/3d/application/foundation-mesh';
 
 export enum IDEApiDest {
   VizDo = 'vizDo',
@@ -52,3 +63,285 @@ export type OrderTuple = {
   hierarchyModel: ParentOrder;
   meshes: { meshNames: string[]; meshIds: string[] };
 };
+
+export function getOrderedParents(dataModel: Application): ParentOrder {
+  const result: ParentOrder = {
+    fqn: dataModel.name,
+    childs: [],
+    meshid: dataModel.id,
+    methods: [],
+  };
+  const temp: ParentOrder[] = [];
+  dataModel.packages.forEach((element) => {
+    const fqn = dataModel.name + '.' + element.name;
+    temp.push({
+      fqn: fqn,
+      childs: parentPackage(fqn, element.subPackages, element.classes),
+      meshid: element.id,
+      methods: [],
+    });
+  });
+
+  result.childs = temp;
+
+  return result;
+}
+
+function parentPackage(
+  fqn: string,
+  subpackages: Package[],
+  classes: Class[]
+): ParentOrder[] {
+  const temp: ParentOrder[] = [];
+
+  if (subpackages.length === 0) {
+    return parentClass(fqn, classes);
+  }
+  subpackages.forEach((element) => {
+    const newFqn = fqn + '.' + element.name;
+    temp.push({
+      fqn: newFqn,
+      childs: parentPackage(newFqn, element.subPackages, element.classes),
+      meshid: element.id,
+      methods: [],
+    });
+  });
+
+  return temp;
+}
+
+function parentClass(fqn: string, classes: Class[]): ParentOrder[] {
+  const temp: ParentOrder[] = [];
+  // console.log(classes)
+  if (classes.length === 0) {
+    return temp;
+  }
+  classes.forEach((element) => {
+    const newFqn = fqn + '.' + element.name;
+    temp.push({
+      fqn: newFqn,
+      childs: [],
+      meshid: element.id,
+      methods: [],
+    });
+  });
+
+  return temp;
+}
+
+function getFqnForMeshes(orderedParents: ParentOrder): {
+  meshNames: string[];
+  meshIds: string[];
+} {
+  const meshName: string = orderedParents.fqn;
+  const meshId: string = orderedParents.meshid;
+
+  const meshTemp = { meshNames: [meshName], meshIds: [meshId] };
+
+  if (orderedParents.methods.length != 0) {
+    orderedParents.methods.forEach((element) => {
+      meshTemp.meshNames = meshTemp.meshNames.concat(
+        getFqnForMeshes(element).meshNames
+      );
+      meshTemp.meshIds = meshTemp.meshIds.concat(
+        getFqnForMeshes(element).meshIds
+      );
+    });
+  } else {
+    orderedParents.childs.forEach((element) => {
+      meshTemp.meshNames = meshTemp.meshNames.concat(
+        getFqnForMeshes(element).meshNames
+      );
+      meshTemp.meshIds = meshTemp.meshIds.concat(
+        getFqnForMeshes(element).meshIds
+      );
+    });
+  }
+
+  return meshTemp;
+}
+
+export function VizDataToOrderTuple(vizData: VizDataRaw): OrderTuple[] {
+  const vizDataOrderTuple: OrderTuple[] = [];
+  vizData.applicationObject3D.forEach((element) => {
+    const orderedParents = getOrderedParents(element.data.application);
+    const meshes = getFqnForMeshes(orderedParents);
+    let tempOT: OrderTuple = { hierarchyModel: orderedParents, meshes: meshes };
+    tempOT = addCommunicationLinksToOrderTuple(
+      tempOT,
+      vizData.communicationLinks
+    );
+    vizDataOrderTuple.push(tempOT);
+  });
+  return vizDataOrderTuple;
+}
+
+export function OpenObject(
+  doSomethingOnMesh: (meshID: string) => void,
+  fullQualifiedName: string,
+  occurrenceID: number,
+  lookAtMesh: (meshID: string) => void,
+  vizData: VizDataRaw
+) {
+  const orderTuple: OrderTuple[] = VizDataToOrderTuple(vizData);
+
+  resetFoundation(doSomethingOnMesh, orderTuple);
+
+  orderTuple.forEach((ot) => {
+    const occurrenceName = occurrenceID == -1 ? '.' : '.' + occurrenceID + '.';
+
+    console.log('ot.hierarchyModel.fqn', ot.hierarchyModel.fqn);
+    recursivelyOpenObjects(
+      doSomethingOnMesh,
+      lookAtMesh,
+      ot.hierarchyModel.fqn + occurrenceName + fullQualifiedName,
+      ot
+    );
+  });
+}
+
+function recursivelyOpenObjects(
+  doSomethingOnMesh: (meshID: string) => void,
+  lookAtMesh: (meshID: string) => void,
+  toOpen: string,
+  orderTuple: OrderTuple
+) {
+  if (orderTuple.meshes.meshNames.indexOf(toOpen) === -1) {
+    return;
+  }
+
+  orderTuple.hierarchyModel.childs.forEach((element) => {
+    const tempOrder: ParentOrder = {
+      fqn: element.fqn,
+      childs: element.childs,
+      meshid: element.meshid,
+      methods: [],
+    };
+    if (element.methods.length != 0) {
+      console.log('Methods elem: ', element);
+    } else if (isInParentOrder(element, toOpen)) {
+      doSomethingOnMesh(element.meshid);
+      if (toOpen == element.fqn) {
+        lookAtMesh(element.meshid);
+      }
+      recursivelyOpenObjects(doSomethingOnMesh, lookAtMesh, toOpen, {
+        hierarchyModel: tempOrder,
+        meshes: orderTuple.meshes,
+      });
+    }
+  });
+}
+
+function isInParentOrder(po: ParentOrder, name: string): boolean {
+  if (po.fqn === name) {
+    return true;
+  } else if (po.childs.length === 0) {
+    return false;
+  }
+  let tempBool = false;
+  po.childs.forEach((element) => {
+    tempBool =
+      tempBool ||
+      isInParentOrder(
+        {
+          fqn: element.fqn,
+          childs: element.childs,
+          meshid: element.meshid,
+          methods: [],
+        },
+        name
+      );
+  });
+
+  return tempBool;
+}
+
+function resetFoundation(
+  doSomethingOnMesh: (meshID: string) => void,
+  orderTuple: OrderTuple[]
+) {
+  orderTuple.forEach((ot) => {
+    doSomethingOnMesh(ot.hierarchyModel.meshid);
+  });
+}
+
+export function getIdFromMesh(mesh: THREE.Object3D<THREE.Event>): string {
+  if (mesh instanceof FoundationMesh) {
+    return mesh.dataModel.id;
+  } else if (mesh instanceof ComponentMesh) {
+    return mesh.dataModel.id;
+  } else if (mesh instanceof ClazzMesh) {
+    console.error('ClazzMesh --- Mesh Type not Supported!');
+    return mesh.dataModel.id;
+  } else if (mesh instanceof ClazzCommunicationMesh) {
+    console.error('ClazzCommunicationMesh --- Mesh Type not Supported!');
+    console.log(mesh.dataModel);
+    return mesh.dataModel.id;
+    // return 'Not implemented ClazzCommunicationMesh';
+  } else if (mesh instanceof CommunicationArrowMesh) {
+    console.error('CommunicationArrowMesh --- Mesh Type not Supported!');
+    return 'Not implemented CommunicationArrowMesh';
+  } else {
+    //
+    console.error(typeof mesh, ' --- Mesh Type not Supported!');
+    return 'Not implemented';
+  }
+}
+
+function addCommunicationLinksToOrderTuple(
+  ot: OrderTuple,
+  communicationLinks: CommunicationLink[]
+): OrderTuple {
+  const tempOT = ot;
+
+  communicationLinks.forEach((cl) => {
+    const communicationLinkFQNIndex = ot.meshes.meshIds.findIndex(
+      (e) => e === cl.targetMeshID
+    );
+    if (communicationLinkFQNIndex >= 0) {
+      const communicationLinkFQN =
+        ot.meshes.meshNames[communicationLinkFQNIndex] + '.' + cl.methodName;
+
+      tempOT.hierarchyModel = insertCommunicationInParentOrder(
+        cl,
+        communicationLinkFQN,
+        tempOT.hierarchyModel
+      );
+      tempOT.meshes.meshNames.push(communicationLinkFQN);
+      tempOT.meshes.meshIds.push(cl.meshID);
+    }
+  });
+
+  return tempOT;
+}
+
+function insertCommunicationInParentOrder(
+  cl: CommunicationLink,
+  communicationLinkFQN: string,
+  po: ParentOrder
+): ParentOrder {
+  if (cl.targetMeshID == po.meshid) {
+    const newPO: ParentOrder = {
+      childs: [],
+      fqn: communicationLinkFQN,
+      meshid: cl.meshID,
+      methods: [],
+    };
+    const tempPO = po;
+    tempPO.childs.push(newPO);
+    return tempPO;
+  } else {
+    const temp: ParentOrder[] = [];
+    po.childs.forEach((element) => {
+      temp.push(
+        insertCommunicationInParentOrder(cl, communicationLinkFQN, element)
+      );
+    });
+    return {
+      fqn: po.fqn,
+      meshid: po.meshid,
+      methods: po.methods,
+      childs: temp,
+    };
+  }
+}
