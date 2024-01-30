dff24378fafdd0a0d6358107ade8b22d4cdde118
Natalie Kaufhold <stu222510@mail.uni-kiel.de>
2024-01-30 22:33:31 +0100

    v4.4.0...v5.6.0 using ember-cli-update. There are still failing ember tests. Want to version this state to be able to come back

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,12 @@
 'use strict';
 
 module.exports = {
-  singleQuote: true,
-  trailingComma: 'es5',
-  printWidth: 80,
+  overrides: [
+    {
+      files: '*.{js,ts}',
+      options: {
+        singleQuote: true,
+      },
+    },
+  ],
 };
