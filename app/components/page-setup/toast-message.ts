8a9b98570c23c23a1ce8bcaca70de68e7a4ad755
Alexander Krause <akr@informatik.uni-kiel.de>
2024-02-19 13:38:12 +0100

    use uuidv4 generator also for toast messages

--- app/components/page-setup/toast-message.ts
+++ app/components/page-setup/toast-message.ts
@@ -4,6 +4,7 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import ToastHandlerService from 'explorviz-frontend/services/toast-handler';
+import generateUuidv4 from 'explorviz-frontend/utils/helpers/uuid4-generator';
 
 export default class PageSetupToastMessageComponent extends Component {
   @service('toastHandler')
@@ -34,7 +35,7 @@ export default class PageSetupToastMessageComponent extends Component {
   }
 
   private addToastMessage(type: string, message: string, header: string) {
-    const htmlIdUnique = 'toast-' + this.uuidv4();
+    const htmlIdUnique = 'toast-' + generateUuidv4();
 
     let cssClasses = this.cssClasses.get(type);
 
@@ -48,16 +49,6 @@ export default class PageSetupToastMessageComponent extends Component {
       message,
       cssClasses,
     });
-  }
-
-  private uuidv4(): string {
-    // https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
-    return '10000000-1000-4000-8000-100000000000'.replace(/[018]/g, (c: any) =>
-      (
-        c ^
-        (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
-      ).toString(16)
-    );
   }
 
   @action
