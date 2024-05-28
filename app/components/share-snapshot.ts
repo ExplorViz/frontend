6055889d8820cd732dcbecaa3443b43edad791f2
Julius <julius.brehme@googlemail.com>
2024-05-28 22:12:05 +0200

    set expiry date when sharing a snapshot

--- app/components/share-snapshot.ts
+++ app/components/share-snapshot.ts
@@ -6,6 +6,8 @@ import Auth from 'explorviz-frontend/services/auth';
 import SnapshotTokenService, {
   SnapshotToken,
 } from 'explorviz-frontend/services/snapshot-token';
+import { tracked } from '@glimmer/tracking';
+import convertDate from 'explorviz-frontend/utils/helpers/time-convter';
 // import ENV from 'explorviz-frontend/config/environment';
 /**
  * TODO: website must reload to show snapshot in right table
@@ -13,6 +15,7 @@ import SnapshotTokenService, {
  */
 
 // const url = ENV.shareSnapshotURL;
+
 export default class ShareSnapshotComponent extends Component<SnapshotToken> {
   @service('auth')
   auth!: Auth;
@@ -23,10 +26,30 @@ export default class ShareSnapshotComponent extends Component<SnapshotToken> {
   @service('toast-handler')
   toastHandlerService!: ToastHandlerService;
 
-  // this needs to be async to make DB call
-  /**
-   * TODO: create URL and edit DB entry
-   */
+  @tracked
+  setExpireDateMenu: boolean = false;
+
+  @tracked
+  expDate: number | null = null;
+
+  @action
+  openMenu() {
+    this.setExpireDateMenu = true;
+  }
+
+  @action
+  closeMenu() {
+    this.setExpireDateMenu = false;
+    this.expDate = null;
+  }
+
+  @action
+  updateExpDate(event: InputEvent) {
+    const target: HTMLInputElement = event.target as HTMLInputElement;
+    const date = convertDate(target.value);
+    this.expDate = date;
+  }
+
   @action
   async shareSnapshot(snapshot: SnapshotToken) {
     this.snapshotService.shareSnapshot(snapshot);
