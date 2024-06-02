d7870a8dd62eda1f47d43ec4bbd960e5844ab25b
Malte Hansen <malte.hanseat@gmail.com>
2024-06-02 20:21:00 +0200

    Move functions to interact with local storage into utility file

---
+++ app/utils/settings/local-storage-settings.ts
@@ -0,0 +1,48 @@
+import isObject, { objectsHaveSameKeys } from '../object-helpers';
+import { defaultApplicationSettings } from './default-settings';
+import { ApplicationSettings, RangeSetting } from './settings-schemas';
+
+export function getStoredSettings() {
+  const userApplicationSettingsJSON = localStorage.getItem(
+    'userApplicationSettings'
+  );
+
+  if (userApplicationSettingsJSON === null) {
+    throw new Error('There are no application settings to restore');
+  }
+
+  const parsedApplicationSettings = JSON.parse(userApplicationSettingsJSON);
+
+  if (areValidApplicationSettings(parsedApplicationSettings)) {
+    return parsedApplicationSettings;
+  } else {
+    localStorage.removeItem('userApplicationSettings');
+    return defaultApplicationSettings;
+  }
+}
+
+export function areValidApplicationSettings(maybeSettings: unknown) {
+  return (
+    isObject(maybeSettings) &&
+    objectsHaveSameKeys(maybeSettings, defaultApplicationSettings)
+  );
+}
+
+export function validateRangeSetting(
+  rangeSetting: RangeSetting,
+  value: number
+) {
+  const { range } = rangeSetting;
+  if (Number.isNaN(value)) {
+    throw new Error('Value is not a number');
+  } else if (value < range.min || value > range.max) {
+    throw new Error(`Value must be between ${range.min} and ${range.max}`);
+  }
+}
+
+export function saveSettings(applicationSettings: ApplicationSettings) {
+  localStorage.setItem(
+    'userApplicationSettings',
+    JSON.stringify(applicationSettings)
+  );
+}
