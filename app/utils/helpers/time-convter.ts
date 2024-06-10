668c633b689cffe12906274bdd86edb9356dd665
Julius <julius.brehme@googlemail.com>
2024-06-10 21:03:22 +0200

    fixed to convert to right month

--- app/utils/helpers/time-convter.ts
+++ app/utils/helpers/time-convter.ts
@@ -9,5 +9,8 @@ export default function convertDate(date: string) {
   const month = date.slice(5, 7);
   const year = date.slice(0, 4);
 
-  return new Date(Date.UTC(Number(year), Number(month), Number(day))).valueOf();
+  // returns always a months ahead. Therefore, substract one month.
+  return new Date(
+    Date.UTC(Number(year), Number(month) - 1, Number(day))
+  ).valueOf();
 }
