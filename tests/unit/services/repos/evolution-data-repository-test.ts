cf4ab24fd75004c23545e873f443d3b93b222f77
Alexander Krause <akr@informatik.uni-kiel.de>
2024-07-15 15:45:12 +0200

    fixed tests

--- tests/unit/services/repos/evolution-data-repository-test.ts
+++ tests/unit/services/repos/evolution-data-repository-test.ts
@@ -1,12 +1,12 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
-module('Unit | Service | repos/evolution-data-repository.ts', function (hooks) {
+module('Unit | Service | repos/evolution-data-repository', function (hooks) {
   setupTest(hooks);
 
   // TODO: Replace this with your real tests.
   test('it exists', function (assert) {
-    let service = this.owner.lookup('service:repos/evolution-data-repository.ts');
+    let service = this.owner.lookup('service:repos/evolution-data-repository');
     assert.ok(service);
   });
 });
