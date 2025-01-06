6e978afaf2161464fa73296d4df0a822db0c7d63
Malte Hansen <malte.hanseat@gmail.com>
2025-01-06 13:56:33 +0100

    First draft for download of JSON files

--- app/components/token-selection.ts
+++ app/components/token-selection.ts
@@ -4,6 +4,8 @@ import { inject as service } from '@ember/service';
 import { LandscapeToken } from 'explorviz-frontend/services/landscape-token';
 import { action } from '@ember/object';
 import Auth from 'explorviz-frontend/services/auth';
+import ENV from 'explorviz-frontend/config/environment';
+const { spanService } = ENV.backendAddresses;
 
 interface Args {
   tokens: LandscapeToken[];
@@ -34,6 +36,59 @@ export default class TokenSelection extends Component<Args> {
     } else {
       this.sortOrder = 'asc';
       this.sortProperty = property;
+    }
+  }
+
+  @action
+  downloadJson(token: LandscapeToken, event: MouseEvent) {
+    event?.stopPropagation();
+    this.downloadJSONFile(
+      `${spanService}/v2/landscapes/${token.value}/structure`,
+      'structure.json'
+    );
+    this.downloadJSONFile(
+      `${spanService}/v2/landscapes/${token.value}/dynamic`,
+      'dynamic.json'
+    );
+    this.downloadJSONFile(
+      `${spanService}/v2/landscapes/${token.value}/timestamps`,
+      'timestamps.json'
+    );
+  }
+
+  async downloadJSONFile(url: string, filename = 'data.json') {
+    try {
+      // Fetch the JSON data from the backend service
+      const response = await fetch(url);
+
+      if (!response.ok) {
+        throw new Error(`Error fetching JSON: ${response.statusText}`);
+      }
+
+      // Parse the response as JSON
+      await response.json();
+
+      // Convert the JSON data to a Blob
+      const jsonBlob = new Blob([], {
+        type: 'application/json',
+      });
+
+      // Create a temporary link element
+      const tempLink = document.createElement('a');
+      tempLink.href = URL.createObjectURL(jsonBlob);
+      tempLink.download = filename;
+
+      // Append the link to the body and trigger the download
+      document.body.appendChild(tempLink);
+      tempLink.click();
+
+      // Clean up: Remove the link and release the object URL
+      document.body.removeChild(tempLink);
+      URL.revokeObjectURL(tempLink.href);
+
+      console.log('JSON file downloaded successfully.');
+    } catch (error) {
+      console.error('Error downloading JSON file:', error);
     }
   }
 }
