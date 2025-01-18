7f3af66e5fac195ebab3bc1d48c98483b4abe349
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-01-18 18:39:58 +0100

    Prepared ar-settings for migration
    Former-commit-id: 2e39b5540b63101f8878180218caf90d093f3d91

--- react-lib/src/stores/extended-reality/ar-settings.ts
+++ react-lib/src/stores/extended-reality/ar-settings.ts
@@ -1,20 +1,38 @@
 import { createStore } from 'zustand/vanilla';
+// import { useHeatmapConfigurationStore } from 'react-lib/src/stores/heatmap/heatmap-configuration';
+// import { useApplicationRendererStore } from 'react-lib/src/stores/application-renderer';
 
 interface ARSettingsState {
-    landscapeOpacity: number;
-    applicationOpacity: number;
+    landscapeOpacity: number; // tracked
+    applicationOpacity: number; // tracked
     sidebarWidthInPercent: number | undefined;
-    renderCommunication: boolean;
-    zoomLevel: number;
-    stackPopups: boolean;
+    renderCommunication: boolean; // tracked
+    zoomLevel: number; // tracked
+    stackPopups: boolean; // tracked
+    // setApplicationOpacity: (opacity: number) => void;
+    // updateApplicationOpacity: () => void;
 }
 
-export const useARSettingsStore = createStore<ARSettingsState>(() => ({
+export const useARSettingsStore = createStore<ARSettingsState>((set, get) => ({
     landscapeOpacity: 0.9,
     applicationOpacity: 0.9,
     sidebarWidthInPercent: undefined,
     renderCommunication: true,
     zoomLevel: 3,
     stackPopups: true,
+
+    // setApplicationOpacity: (opacity: number) => {
+    //     set({ applicationOpacity:  opacity });
+    //     if (!useHeatmapConfigurationStore.getState().heatmapActive) {
+    //       get().updateApplicationOpacity();
+    //     }
+    // },
+
+    // updateApplicationOpacity: () => {
+    //     useApplicationRendererStore.getState().getOpenApplications()
+    //       .forEach((applicationObject3D) => {
+    //         applicationObject3D.setOpacity(get().applicationOpacity);
+    //       });
+    // },
 }));
 
