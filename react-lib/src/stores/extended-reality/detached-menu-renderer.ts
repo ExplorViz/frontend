ae7e300a778b80a7add5b17f169098c2f15f6270
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-01-18 18:25:35 +0100

    Prepared detached-menu-renderer for migration
    Former-commit-id: 787f1f43c700d459d228e6a819b9dc789a0a375e

--- react-lib/src/stores/extended-reality/detached-menu-renderer.ts
+++ react-lib/src/stores/extended-reality/detached-menu-renderer.ts
@@ -1,1 +1,100 @@
-// TODO: Delete if file not necessary, otherwise fill when clear
+// // TODO: Use after services and utils fully migrated
+
+// import {
+//   getTypeOfEntity,
+//   isEntityMesh,
+// } from 'explorviz-frontend/utils/extended-reality/vr-helpers/detail-info-composer';
+// import {
+//   SerializedAnnotation,
+//   SerializedDetachedMenu,
+//   SerializedPopup,
+// } from 'react-lib/src/utils/collaboration/web-socket-messages/types/serialized-room';
+// import { SPECTATE_VIEW_ENTITY_TYPE } from 'react-lib/src/utils/collaboration/web-socket-messages/types/entity-type';
+// import eventEmitter from 'react-lib/src/utils/event-emitter';
+// import { useLocalUserStore } from 'react-lib/src/stores/collaboration/local-user';
+// import { useApplicationRendererStore } from 'react-lib/src/stores/application-renderer';
+// import { useVrMenuFactoryStore } from 'react-lib/src/stores/extended-reality/vr-menu-factory';
+// import { useDetachedMenuGroupsStore } from 'react-lib/src/stores/extended-reality/detached-menu-groups';
+
+// export default class DetachedMenuRenderer {
+//     restore(popups: SerializedPopup[], detachedMenus: SerializedDetachedMenu[]) {
+//         if (useLocalUserStore.getState().visualizationMode === 'browser') {
+//         const popupsFromMenu: SerializedPopup[] = detachedMenus.map(
+//             (detachedMenu) => {
+//             return {
+//                 userId: detachedMenu.userId,
+//                 entityId: detachedMenu.entityId,
+//                 menuId: detachedMenu.objectId,
+//             };
+//             }
+//         );
+//         eventEmitter.emit('restore_popups', popups.concat(popupsFromMenu));
+//         }
+
+//         if (useLocalUserStore.getState().visualizationMode === 'vr') {
+//         popups.forEach((popup) => {
+//             this.restoreFromPopup(popup);
+//         });
+//         detachedMenus.forEach((detachedMenu) => {
+//             this.restoreDetachedMenu(detachedMenu);
+//         });
+//         }
+//     }
+
+//   restoreAnnotations(annotations: SerializedAnnotation[]) {
+//     for (const an of annotations) {
+//       an.menuId = an.objectId;
+//     }
+//     eventEmitter.emit('restore_annotations', annotations);
+//   }
+
+//   restoreDetachedMenus(detachedMenus: SerializedDetachedMenu[]) {
+//     detachedMenus.forEach((detachedMenu) => {
+//       this.restoreDetachedMenu(detachedMenu);
+//     });
+//   }
+
+//   restoreDetachedMenu(detachedMenu: SerializedDetachedMenu) {
+//     if (detachedMenu.entityType === SPECTATE_VIEW_ENTITY_TYPE) {
+//       return;
+//     }
+//     const object = useApplicationRendererStore.getState().getMeshById(detachedMenu.entityId);
+
+//     if (isEntityMesh(object)) {
+//       const menu = useVrMenuFactoryStore.getState().buildInfoMenu(object);
+//       menu.position.fromArray(detachedMenu.position);
+//       menu.quaternion.fromArray(detachedMenu.quaternion);
+//       menu.scale.fromArray(detachedMenu.scale);
+//       useDetachedMenuGroupsStore.getState().addDetachedMenuLocally(
+//         menu,
+//         detachedMenu.objectId,
+//         detachedMenu.userId
+//       );
+//     }
+//   }
+
+//   restoreFromPopups(popupData: SerializedPopup[]) {
+//     popupData.forEach((popup) => {
+//       this.restoreFromPopup(popup);
+//     });
+//   }
+
+//   restoreFromPopup(popupData: SerializedPopup) {
+//     const mesh = useApplicationRendererStore.getState().getMeshById(popupData.entityId);
+//     if (!isEntityMesh(mesh)) {
+//       return;
+//     }
+//     const worldPosition = useApplicationRendererStore.getState().getGraphPosition(mesh);
+//     worldPosition.y += 0.3;
+
+//     this.restoreDetachedMenu({
+//       objectId: popupData.entityId,
+//       userId: null,
+//       entityId: popupData.entityId,
+//       entityType: getTypeOfEntity(mesh),
+//       position: worldPosition.toArray(),
+//       quaternion: [0, 0, 0, 0],
+//       scale: [1, 1, 1],
+//     });
+//   }
+// }
