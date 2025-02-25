513395e25d904facbab2b54bb50ba5a303750ac7
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-02-25 11:06:46 +0100

    Migrate component: edit-mesh
    Former-commit-id: 87da3e320d5727e32cb4678fa5a12f909e69e550

---
+++ react-lib/src/components/visualization/rendering/popups/interactable-properties/edit-mesh.tsx
@@ -0,0 +1,191 @@
+import React, { useState } from 'react';
+
+import { useUserSettingsStore } from 'react-lib/src/stores/user-settings';
+import { useLandscapeRestructureStore } from 'react-lib/src/stores/landscape-restructure';
+import {
+  isApplication,
+  isClass,
+  isPackage,
+} from 'react-lib/src/utils/landscape-schemes/structure-data';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+import {
+  CopyIcon,
+  DiffRenamedIcon,
+  DuplicateIcon,
+  PackageIcon,
+  TrashIcon,
+} from '@primer/octicons-react';
+
+interface EditMeshProps {
+  entity: any;
+  appId: string;
+}
+
+export default function EditMesh({ entity, appId }: EditMeshProps) {
+  const deletedDataModels = useLandscapeRestructureStore(
+    (state) => state.deletedDataModels
+  );
+
+  const clippedMesh = useLandscapeRestructureStore(
+    (state) => state.clippedMesh
+  );
+
+  const [packageColor, setPackageColor] = useState<string>(
+    useUserSettingsStore.getState().visualizationSettings.componentOddColor
+      .value
+  );
+
+  const [clazzColor, setClazzColor] = useState<string>(
+    useUserSettingsStore.getState().visualizationSettings.clazzColor.value
+  );
+
+  const isEntityApplication = isApplication(entity);
+  const isEntityPackage = isPackage(entity);
+  const isEntityClass = isClass(entity);
+  const isDeleted = !(
+    (isEntityApplication &&
+      deletedDataModels.some((deletedEntity) => deletedEntity === entity)) ||
+    (isEntityPackage &&
+      deletedDataModels.some((deletedEntity) => deletedEntity === entity)) ||
+    (isEntityClass &&
+      deletedDataModels.some((deletedEntity) => deletedEntity === entity))
+  );
+
+  const isInsertable: boolean = (() => {
+    if (!clippedMesh) return false;
+
+    const isClippedMeshPackage = isPackage(clippedMesh);
+    const isEntityApplicationOrPackage =
+      isApplication(entity) || isPackage(entity);
+
+    const isClippedMeshClass = isClass(clippedMesh);
+    const isEntityPackage = isPackage(entity);
+
+    return (
+      (isClippedMeshPackage && isEntityApplicationOrPackage) ||
+      (isClippedMeshClass && isEntityPackage)
+    );
+  })();
+
+  const duplicateApp = () => {
+    useLandscapeRestructureStore.getState().duplicateApp(entity);
+  };
+
+  const addPackage = () => {
+    if (isEntityApplication)
+      useLandscapeRestructureStore.getState().addPackage(entity);
+    else if (isEntityPackage)
+      useLandscapeRestructureStore.getState().addSubPackage(entity);
+  };
+
+  const addClass = () => {
+    if (isEntityPackage) {
+      useLandscapeRestructureStore.getState().addClass(entity);
+    }
+  };
+
+  const deleteMesh = () => {
+    if (isEntityApplication)
+      useLandscapeRestructureStore.getState().deleteApp(entity);
+    else if (isEntityPackage)
+      useLandscapeRestructureStore.getState().deletePackage(entity);
+    else if (isEntityClass)
+      useLandscapeRestructureStore.getState().deleteClass(entity);
+  };
+
+  const cutMesh = () => {
+    if (isEntityPackage)
+      useLandscapeRestructureStore.getState().cutPackage(entity);
+    else if (isEntityClass)
+      useLandscapeRestructureStore.getState().cutClass(entity);
+  };
+
+  const setCommunicationSource = () => {
+    useLandscapeRestructureStore.getState().setCommunicationSourceClass(entity);
+  };
+
+  const setCommunicationTarget = () => {
+    useLandscapeRestructureStore.getState().setCommunicationTargetClass(entity);
+  };
+
+  const insertMesh = () => {
+    useLandscapeRestructureStore.getState().movePackageOrClass(entity);
+  };
+
+  const pasteMesh = () => {
+    if (isPackage(clippedMesh))
+      useLandscapeRestructureStore.getState().pastePackage(entity);
+    else if (isClass(clippedMesh))
+      useLandscapeRestructureStore.getState().pasteClass(entity);
+  };
+
+  if (isDeleted) {
+    return <></>;
+  }
+
+  const elemPackage = (
+    <>
+      <OverlayTrigger
+        placement="top"
+        trigger={['hover', 'focus']}
+        overlay={<Tooltip>Add Package</Tooltip>}
+      >
+        <Button
+          style={{
+            backgroundColor: packageColor,
+            borderColor: packageColor,
+          }}
+          onClick={addPackage}
+        >
+          <PackageIcon size="small" />
+        </Button>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="top"
+        trigger={['hover', 'focus']}
+        overlay={<Tooltip>Delete</Tooltip>}
+      >
+        <Button variant="danger" onClick={deleteMesh}>
+          <TrashIcon size="small" className="align-right" />
+        </Button>
+      </OverlayTrigger>
+
+      <OverlayTrigger
+        placement="top"
+        trigger={['hover', 'focus']}
+        overlay={<Tooltip>Copy</Tooltip>}
+      >
+        <Button variant="dark" onClick={cutMesh}>
+          <CopyIcon size="small" className="align-right" />
+        </Button>
+      </OverlayTrigger>
+
+      {isInsertable && (
+        <>
+          <OverlayTrigger
+            placement="top"
+            trigger={['hover', 'focus']}
+            overlay={<Tooltip>Paste</Tooltip>}
+          >
+            <Button variant="light" onClick={pasteMesh}>
+              <DuplicateIcon size="small" className="align-right" />
+            </Button>
+          </OverlayTrigger>
+
+          <OverlayTrigger
+            placement="top"
+            trigger={['hover', 'focus']}
+            overlay={<Tooltip>Paste</Tooltip>}
+          >
+            <Button variant="warning" onClick={insertMesh}>
+              <DiffRenamedIcon size="small" className="align-right" />
+            </Button>
+          </OverlayTrigger>
+        </>
+      )}
+    </>
+  );
+}
