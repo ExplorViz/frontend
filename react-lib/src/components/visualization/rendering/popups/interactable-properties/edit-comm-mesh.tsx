f58a69243f5abe54f55d56e2b81528f2a7240780
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-02-25 11:52:04 +0100

    Migrate component: edit-comm-mesh
    Former-commit-id: a08b01c1a5efc2fba6887177242b6cd47a2f5926

---
+++ react-lib/src/components/visualization/rendering/popups/interactable-properties/edit-comm-mesh.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+import ClassCommunication from 'react-lib/src/utils/landscape-schemes/dynamic/class-communication';
+import { useLandscapeRestructureStore } from 'react-lib/src/stores/landscape-restructure';
+import { TrashIcon } from '@primer/octicons-react';
+import Button from 'react-bootstrap/Button';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import Tooltip from 'react-bootstrap/Tooltip';
+
+interface EditCommMeshProps {
+  classCommunication: ClassCommunication;
+}
+
+export default function EditCommMesh({
+  classCommunication,
+}: EditCommMeshProps) {
+  const deletedDataModels = useLandscapeRestructureStore(
+    (state) => state.deletedDataModels
+  );
+
+  const deleteCommunication = useLandscapeRestructureStore(
+    (state) => state.deleteCommunication
+  );
+
+  const deleteComm = () => {
+    deleteCommunication(classCommunication);
+  };
+
+  const isDeleted = !deletedDataModels.some(
+    (entity) => entity === classCommunication
+  );
+
+  return (
+    isDeleted && (
+      <tr>
+        <td className="text-nowrap align-top"></td>
+        <td className="text-right text-break pl-1">
+          <OverlayTrigger
+            placement="top"
+            trigger={['hover', 'focus']}
+            overlay={<Tooltip>Delete</Tooltip>}
+          >
+            <Button variant="danger" onClick={deleteComm}>
+              <TrashIcon size="small" className="align-right" />
+            </Button>
+          </OverlayTrigger>
+        </td>
+      </tr>
+    )
+  );
+}
