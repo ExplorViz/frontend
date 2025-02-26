0dca3054caf894ca19274b161b2a5129e05ae4e8
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-02-26 18:46:48 +0100

    copy-button: Add successMessage prop, styling
    Former-commit-id: a99ac4cf7b35a0d9684b1db029c0148b6039db87

--- react-lib/src/components/copy-button.tsx
+++ react-lib/src/components/copy-button.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import { useToastHandlerStore } from 'react-lib/src/stores/toast-handler';
+
 import Tooltip from 'react-bootstrap/Tooltip';
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import { CopyIcon } from '@primer/octicons-react';
+import { useToastHandlerStore } from 'react-lib/src/stores/toast-handler';
 
-export default function CopyButton({ text }: { text: string }) {
+interface CopyButtonProps {
+  text: string;
+  successMessage?: string;
+}
+
+export default function CopyButton({
+  text,
+  successMessage = `${text} copied to clipboard`,
+}: CopyButtonProps) {
+  const showSuccessToastMessage = useToastHandlerStore(
+    (state) => state.showSuccessToastMessage
+  );
+
   const onCopy = () => {
     navigator.clipboard.writeText(text);
-    useToastHandlerStore
-      .getState()
-      .showSuccessToastMessage(text + ' copied to clipboard');
+    showSuccessToastMessage(successMessage);
   };
 
   return (
-    <button className="btn" onClick={() => onCopy()}>
+    <button className="btn button-svg-with-hover" onClick={onCopy}>
       <OverlayTrigger
         placement={'right'}
         trigger={['hover', 'focus']}
         overlay={<Tooltip>Copy to clipboard</Tooltip>}
       >
-        <CopyIcon verticalAlign="middle" size="small" fill="#777" />
+        <CopyIcon verticalAlign="middle" size="small" />
       </OverlayTrigger>
     </button>
   );
--- react-lib/src/stores/toast-handler.ts
+++ react-lib/src/stores/toast-handler.ts
@@ -1,6 +1,6 @@
-import { createStore } from "zustand/vanilla";
+import { create } from 'zustand';
 
-import eventEmitter from "react-lib/src/utils/event-emitter";
+import eventEmitter from 'react-lib/src/utils/event-emitter';
 
 interface ToastHandlerState {
   showInfoToastMessage: (message: string, header?: string) => void;
@@ -8,16 +8,16 @@ interface ToastHandlerState {
   showErrorToastMessage: (message: string, header?: string) => void;
 }
 
-export const useToastHandlerStore = createStore<ToastHandlerState>(() => ({
-  showInfoToastMessage: (message: string, header: string = "") => {
-    eventEmitter.emit("newToastMessage", "info", message, header);
+export const useToastHandlerStore = create<ToastHandlerState>(() => ({
+  showInfoToastMessage: (message: string, header: string = '') => {
+    eventEmitter.emit('newToastMessage', 'info', message, header);
   },
 
-  showSuccessToastMessage: (message: string, header: string = "") => {
-    eventEmitter.emit("newToastMessage", "success", message, header);
+  showSuccessToastMessage: (message: string, header: string = '') => {
+    eventEmitter.emit('newToastMessage', 'success', message, header);
   },
 
-  showErrorToastMessage: (message: string, header: string = "") => {
-    eventEmitter.emit("newToastMessage", "error", message, header);
+  showErrorToastMessage: (message: string, header: string = '') => {
+    eventEmitter.emit('newToastMessage', 'error', message, header);
   },
 }));
