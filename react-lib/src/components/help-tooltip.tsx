314656075abc4b91562756d4e250dd10f5548d1e
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-02-26 18:48:24 +0100

    help-tooltip: Move OverlayTrigger to parent div, streamline component definition
    Former-commit-id: ef6d918b1840986326b4763fca292b87f8bfdeb8

--- react-lib/src/components/help-tooltip.tsx
+++ react-lib/src/components/help-tooltip.tsx
@@ -1,25 +1,25 @@
-import React from 'react'; // Unused, but suppresses Typescript warning
+import React from 'react';
 
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
 import Tooltip from 'react-bootstrap/Tooltip';
 import { Placement } from 'react-bootstrap/types';
 import { InfoIcon } from '@primer/octicons-react';
 
-// TODO import .scss
-
-export default function HelpTooltip(args: {
+interface HelpTooltipProps {
   title: String;
   placement?: Placement;
-}) {
+}
+
+export default function HelpTooltip({ title, placement }: HelpTooltipProps) {
   return (
-    <div className="inline" style={{ marginRight: '0.25rem' }}>
-      <OverlayTrigger
-        placement={args.placement ?? 'left'}
-        trigger={['hover', 'focus']}
-        overlay={<Tooltip>{args.title}</Tooltip>}
-      >
+    <OverlayTrigger
+      placement={placement ?? 'left'}
+      trigger={['hover', 'focus']}
+      overlay={<Tooltip>{title}</Tooltip>}
+    >
+      <div className="inline" style={{ marginRight: '0.25rem' }}>
         <InfoIcon verticalAlign="middle" size="small" fill="#777" />
-      </OverlayTrigger>
-    </div>
+      </div>
+    </OverlayTrigger>
   );
 }
