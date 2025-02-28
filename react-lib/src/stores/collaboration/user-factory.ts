7a0b0f9de4525cee28fa28241d5b4d22813eeb6c
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-02-28 14:45:12 +0100

    Updated user-factory store
    Former-commit-id: b957aed95d4f8589f23e81636c0338cafdb87c8b

--- react-lib/src/stores/collaboration/user-factory.ts
+++ react-lib/src/stores/collaboration/user-factory.ts
@@ -1,60 +1,61 @@
-import { createStore } from "zustand/vanilla";
-
-import * as THREE from "three";
-import { Color } from "react-lib/src/utils/collaboration/web-socket-messages/types/color";
-import { Position } from "react-lib/src/utils/collaboration/web-socket-messages/types/position";
-import { Quaternion } from "react-lib/src/utils/collaboration/web-socket-messages/types/quaternion";
-
-import { useHMDStore } from "react-lib/src/stores/extended-reality/hmd";
+import { create } from 'zustand';
+import RemoteUser from 'react-lib/src/utils/collaboration/remote-user';
+import * as THREE from 'three';
+import { Color } from 'react-lib/src/utils/collaboration/web-socket-messages/types/color';
+import { Position } from 'react-lib/src/utils/collaboration/web-socket-messages/types/position';
+import { Quaternion } from 'react-lib/src/utils/collaboration/web-socket-messages/types/quaternion';
+import { useLocalUserStore } from './local-user';
+import { useHMDStore } from 'react-lib/src/stores/extended-reality/hmd';
+import { useMinimapStore } from 'react-lib/src/stores/minimap-service';
 
 // TODO this could probably just be a utility
 // TODO wait until RemoteUser is migrated
 
 interface UserFactoryState {
-  //   createUser: ({
-  //     userName,
-  //     userId,
-  //     color,
-  //     position,
-  //     quaternion,
-  //   }: {
-  //     userName: string;
-  //     userId: string;
-  //     color: Color;
-  //     position: Position;
-  //     quaternion: Quaternion;
-  //   }) => RemoteUser;
+  createUser: ({
+    userName,
+    userId,
+    color,
+    position,
+    quaternion,
+  }: {
+    userName: string;
+    userId: string;
+    color: Color;
+    position: Position;
+    quaternion: Quaternion;
+  }) => RemoteUser;
 }
 
-export const useUserFactoryStore = createStore<UserFactoryState>(
-  (set, get) => ({
-    // createUser: ({
-    //   userName,
-    //   userId,
-    //   color,
-    //   position,
-    //   quaternion,
-    // }: {
-    //   userName: string;
-    //   userId: string;
-    //   color: Color;
-    //   position: Position;
-    //   quaternion: Quaternion;
-    // }) => {
-    //   const remoteUser = new RemoteUser({
-    //     userName,
-    //     userId,
-    //     color: new THREE.Color(color.red, color.green, color.blue),
-    //     state: "online",
-    //     // TODO local-user and minimap-service should not be passed as arguments
-    //     // in RemoteUser constructor
-    //   });
-    //   useHMDStore
-    //     .getState()
-    //     .headsetModel.then((hmd) =>
-    //       remoteUser.initCamera(hmd.clone(true), { position, quaternion })
-    //     );
-    //   return remoteUser;
-    // },
-  })
-);
+export const useUserFactoryStore = create<UserFactoryState>((set, get) => ({
+  createUser: ({
+    userName,
+    userId,
+    color,
+    position,
+    quaternion,
+  }: {
+    userName: string;
+    userId: string;
+    color: Color;
+    position: Position;
+    quaternion: Quaternion;
+  }) => {
+    const remoteUser = new RemoteUser({
+      userName,
+      userId,
+      color: new THREE.Color(color.red, color.green, color.blue),
+      state: 'online',
+      // TODO: local-user and minimap-service should not be passed as arguments
+      // in RemoteUser constructor. CHANGE THIS
+      localUser: useLocalUserStore,
+      minimapService: useMinimapStore,
+    });
+    useHMDStore
+      .getState()
+      .headsetModel.then((hmd) =>
+        remoteUser.initCamera(hmd.clone(true), { position, quaternion })
+      );
+    return remoteUser;
+  },
+}));
