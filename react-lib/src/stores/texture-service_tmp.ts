7ed5fc59dcceb32a545ba080b8eca949d34fafc8
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-02-28 14:40:10 +0100

    Added finished texture-service store as temp file
    Former-commit-id: 66f6a1898878f7cc3b9a5ca8d5a250ba96cfd0fd

---
+++ react-lib/src/stores/texture-service_tmp.ts
@@ -0,0 +1,127 @@
+import { create } from 'zustand';
+import ClazzCommunicationMesh from 'react-lib/src/view-objects/3d/application/clazz-communication-mesh';
+import BaseMesh from 'react-lib/src/view-objects/3d/base-mesh.ts';
+import {
+  EntityMesh,
+  isEntityMesh,
+} from 'react-lib/src/utils/extended-reality/vr-helpers/detail-info-composer';
+import { Texture, TextureLoader } from 'three';
+
+export enum TextureNameConstants {
+  ADDED = 'plus',
+  DELETED = 'minus',
+  MODIFIED = 'hashtag',
+}
+
+interface TextureServiceState {
+  _textureCache: Map<string, Texture>;
+  loader: TextureLoader;
+  _constructor: () => void;
+  addTexture: (texturePath: string, texture: Texture) => void;
+  _loadTexture: (
+    texturePath: string,
+    onLoad?: (texture: Texture) => void
+  ) => void;
+  applyAddedTextureToMesh: (mesh: BaseMesh | undefined) => void;
+  applyDeletedTextureToMesh: (mesh: BaseMesh | undefined) => void;
+  applyModifiedTextureToMesh: (mesh: BaseMesh | undefined) => void;
+}
+
+export const useTextureServiceStore = create<TextureServiceState>(
+  (set, get) => ({
+    _textureCache: new Map(),
+    addTexture,
+    loader: new TextureLoader(),
+
+    _constructor: () => {
+      get()._loadTexture(TextureNameConstants.ADDED);
+      get()._loadTexture(TextureNameConstants.DELETED);
+      get()._loadTexture(TextureNameConstants.MODIFIED);
+    },
+
+    _loadTexture: (
+      texturePath: string,
+      onLoad?: (texture: Texture) => void
+    ) => {
+      const filenamePrefix = '../images/';
+      const filenameSuffix = '.png';
+
+      // use callback due to asynchronous nature
+      if (get()._textureCache.has(texturePath)) {
+        if (onLoad) {
+          onLoad(get()._textureCache.get(texturePath)!);
+        }
+        return;
+      }
+
+      get().loader.load(
+        filenamePrefix + texturePath + filenameSuffix,
+        (texture) => {
+          get().addTexture(texturePath, texture);
+          if (onLoad) {
+            onLoad(texture);
+          }
+        }
+      );
+    },
+
+    applyAddedTextureToMesh: (mesh: BaseMesh | undefined) => {
+      if (!isEntityMesh(mesh)) {
+        return;
+      }
+
+      get()._loadTexture(TextureNameConstants.ADDED, (loadedTexture) => {
+        if (mesh instanceof ClazzCommunicationMesh) {
+          const start = mesh.layout.startPoint;
+          const end = mesh.layout.endPoint;
+          const dist = start.distanceTo(end);
+          (mesh as EntityMesh).changeTexture(loadedTexture, Math.ceil(dist), 3); // TODO: wrong?
+        } else {
+          mesh.changeTexture(loadedTexture);
+        }
+      });
+    },
+
+    applyDeletedTextureToMesh: (mesh: BaseMesh | undefined) => {
+      if (!isEntityMesh(mesh)) {
+        return;
+      }
+
+      get()._loadTexture(TextureNameConstants.DELETED, (loadedTexture) => {
+        if (mesh instanceof ClazzCommunicationMesh) {
+          const start = mesh.layout.startPoint;
+          const end = mesh.layout.endPoint;
+          const dist = start.distanceTo(end);
+          (mesh as EntityMesh).changeTexture(loadedTexture, Math.ceil(dist), 3); // TODO: wrong?
+        } else {
+          mesh.changeTexture(loadedTexture);
+        }
+      });
+    },
+
+    applyModifiedTextureToMesh: (mesh: BaseMesh | undefined) => {
+      if (!isEntityMesh(mesh)) {
+        return;
+      }
+
+      get()._loadTexture(TextureNameConstants.MODIFIED, (loadedTexture) => {
+        if (mesh instanceof ClazzCommunicationMesh) {
+          const start = mesh.layout.startPoint;
+          const end = mesh.layout.endPoint;
+          const dist = start.distanceTo(end);
+          (mesh as EntityMesh).changeTexture(loadedTexture, Math.ceil(dist), 3); // TODO: wrong?
+        } else {
+          mesh.changeTexture(loadedTexture);
+        }
+      });
+    },
+  })
+);
+
+function addTexture(texturePath: string, texture: Texture) {
+  useTextureServiceStore.setState((prev) => ({
+    _textureCache: new Map(prev._textureCache).set(texturePath, texture),
+  }));
+}
+
+useTextureServiceStore.getState()._constructor();
