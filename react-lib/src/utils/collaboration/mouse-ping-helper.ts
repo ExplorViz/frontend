edd4d3078d66adca2403ac101b6c021e928f55cc
Jaysun Westenberg <jaysunwestenberg@yahoo.de>
2025-03-02 20:40:06 +0100

    Migrated util: mouse-ping-helper
    Former-commit-id: 35fae08efe367ac828d0c65cbe15b0eb5ca1817c

--- react-lib/src/utils/collaboration/mouse-ping-helper.ts
+++ react-lib/src/utils/collaboration/mouse-ping-helper.ts
@@ -19,6 +19,33 @@ export default class MousePing {
     this.mesh.layers.enable(SceneLayers.Ping);
   }
 
+  ping(
+    parentObj: THREE.Object3D,
+    position: THREE.Vector3,
+    durationInMs: number,
+    replay = false
+  ) {
+    if (this.mesh) {
+      this.mesh.parent?.remove(this.mesh);
+    }
+
+    const worldScale = new THREE.Vector3();
+    parentObj.getWorldScale(worldScale);
+    // disable for floor and other unscaled objects
+    if (worldScale.x === 1) {
+      return;
+    }
+
+    this.mesh.position.copy(position);
+    parentObj.add(this.mesh);
+    this.mesh.startPinging(replay);
+    // await timeout(durationInMs);
+    this.mesh.stopPinging();
+
+    this.mesh.geometry.dispose();
+    (this.mesh.material as THREE.Material).dispose();
+    this.mesh.parent?.remove(this.mesh);
+  }
   // ping = task(
   //   { restartable: true },
   //   async ({
@@ -55,6 +82,42 @@ export default class MousePing {
   //   }
   // );
 
+  pingNonRestartable(
+    parentObj: THREE.Object3D,
+    position: THREE.Vector3,
+    durationInMs: number
+  ) {
+    if (this.meshes.get(position)) {
+      return;
+    }
+
+    const animationMixer = this.animationMixer;
+    const color = this.color;
+
+    const mesh = new PingMesh({
+      animationMixer,
+      color,
+    });
+
+    const worldScale = new THREE.Vector3();
+    parentObj.getWorldScale(worldScale);
+    // disable for floor and other unscaled objects
+    if (worldScale.x === 1) {
+      return;
+    }
+
+    mesh.position.copy(position);
+    parentObj.add(mesh);
+    this.meshes.set(position, mesh);
+    mesh.startPinging();
+    // await timeout(durationInMs);
+    mesh.stopPinging();
+
+    mesh.geometry.dispose();
+    (mesh.material as THREE.Material).dispose();
+    mesh.parent?.remove(mesh);
+    this.meshes.delete(position);
+  }
   // pingNonRestartable = task(
   //   async ({
   //     parentObj,
