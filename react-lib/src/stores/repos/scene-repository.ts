fef60ab4b42465eb0311b633a8d099a05673fda8
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-03-04 11:19:52 +0100

    Updated scene-repository store
    Former-commit-id: b087caee80ce40a71bb7e256fbd82536e26be752

--- react-lib/src/stores/repos/scene-repository.ts
+++ react-lib/src/stores/repos/scene-repository.ts
@@ -1,25 +1,25 @@
-import { createStore } from "zustand/vanilla";
-import { VisualizationMode } from "react-lib/src/stores/collaboration/local-user";
-import { createScene } from "react-lib/src/utils/scene";
+import { create } from 'zustand';
+import { VisualizationMode } from 'react-lib/src/stores/collaboration/local-user';
+import { createScene } from 'react-lib/src/utils/scene';
 
 interface SceneRepositoryState {
   scene?: THREE.Scene;
   getScene: (mode: VisualizationMode, replaceScene: boolean) => THREE.Scene;
 }
 
-export const useSceneRepositoryStore = createStore<SceneRepositoryState>(
+export const useSceneRepositoryStore = create<SceneRepositoryState>(
   (set, get) => ({
     scene: undefined,
+
     getScene: (
-      mode: VisualizationMode = "browser",
+      mode: VisualizationMode = 'browser',
       replaceScene: boolean = false
     ) => {
-      const state = get();
-      if (!state.scene || replaceScene) {
-        state.scene = createScene(mode);
+      if (!get().scene || replaceScene) {
+        set({ scene: createScene(mode) });
       }
 
-      return state.scene;
+      return get().scene!;
     },
   })
 );
