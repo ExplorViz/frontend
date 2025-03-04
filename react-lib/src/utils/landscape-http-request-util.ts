16c624b9d3639fdfd8dfaa6cc5f573ac4d801d1f
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-03-04 13:07:19 +0100

    Refactored landscape-http-request-util store to utils
    Former-commit-id: 7a26b53cd531f2fd1766d38dd3302e20c40c710b

---
+++ react-lib/src/utils/landscape-http-request-util.ts
@@ -0,0 +1,80 @@
+import { StructureLandscapeData } from 'react-lib/src/utils/landscape-schemes/structure-data';
+import { DynamicLandscapeData } from 'react-lib/src/utils/landscape-schemes/dynamic/dynamic-data';
+import * as ENV from 'react-lib/src/env';
+import { useAuthStore } from 'react-lib/src/stores/auth';
+import { useLandscapeTokenStore } from 'react-lib/src/stores/landscape-token';
+
+const spanService = ENV.SPAN_SERV_URL;
+
+export async function requestData(
+  startTime: number,
+  intervalInSeconds: number
+) {
+  const endTime = startTime + intervalInSeconds * 1000;
+
+  const structureDataPromise = requestStructureData(/* startTime, endTime */);
+  const dynamicDataPromise = requestDynamicData(startTime, endTime);
+
+  const landscapeData = Promise.allSettled([
+    structureDataPromise,
+    dynamicDataPromise,
+  ]);
+
+  return landscapeData;
+}
+
+export function requestStructureData(/* fromTimestamp: number, toTimestamp: number */) {
+  return new Promise<StructureLandscapeData>((resolve, reject) => {
+    if (useLandscapeTokenStore.getState().token === null) {
+      reject(new Error('No landscape token selected'));
+      return;
+    }
+    fetch(
+      `${spanService}/v2/landscapes/${useLandscapeTokenStore.getState().token!.value}/structure`,
+      {
+        headers: {
+          Authorization: `Bearer ${useAuthStore.getState().accessToken}`,
+          'Access-Control-Allow-Origin': '*',
+        },
+      }
+    )
+      .then(async (response: Response) => {
+        if (response.ok) {
+          const structureData =
+            (await response.json()) as StructureLandscapeData;
+          structureData.k8sNodes = structureData.k8sNodes || [];
+          resolve(structureData);
+        } else {
+          reject();
+        }
+      })
+      .catch((e) => reject(e));
+  });
+}
+
+export function requestDynamicData(fromTimestamp: number, toTimestamp: number) {
+  return new Promise<DynamicLandscapeData>((resolve, reject) => {
+    if (useLandscapeTokenStore.getState().token === null) {
+      reject(new Error('No landscape token selected'));
+      return;
+    }
+    fetch(
+      `${spanService}/v2/landscapes/${useLandscapeTokenStore.getState().token!.value}/dynamic?from=${fromTimestamp}&to=${toTimestamp}`,
+      {
+        headers: {
+          Authorization: `Bearer ${useAuthStore.getState().accessToken}`,
+          'Access-Control-Allow-Origin': '*',
+        },
+      }
+    )
+      .then(async (response: Response) => {
+        if (response.ok) {
+          const dynamicData = (await response.json()) as DynamicLandscapeData;
+          resolve(dynamicData);
+        } else {
+          reject();
+        }
+      })
+      .catch((e) => reject(e));
+  });
+}
