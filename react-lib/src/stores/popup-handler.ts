baa391b2c94b7bbc7192abb7d839b9c79e10b4a5
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-03-06 11:01:08 +0100

    Finish turning popup-handler into store
    Former-commit-id: c71c1f1e221ba3b0d833ac80cb52a109e64b45dc

--- react-lib/src/stores/popup-handler.ts
+++ react-lib/src/stores/popup-handler.ts
@@ -47,6 +47,50 @@ interface PopupHandlerState {
   latestMousePosition: { timestamp: number; x: number; y: number };
   isShiftPressed: boolean;
   _constructor: () => void;
+  clearPopups: () => void;
+  removeUnpinnedPopups: () => void;
+  removeUnmovedPopups: () => void;
+  sharePopup: (popup: PopupData) => void;
+  pinPopup: (popup: PopupData) => void;
+  removePopup: (entityId: string) => Promise<void>;
+  handleMouseMove: (event: MouseEvent) => void;
+  handleHoverOnMesh: (mesh?: THREE.Object3D) => void;
+  addPopup: ({
+    mesh,
+    position,
+    wasMoved,
+    pinned,
+    menuId,
+    sharedBy,
+    hovered,
+  }: {
+    mesh: THREE.Object3D;
+    position?: Position2D;
+    wasMoved?: boolean;
+    pinned?: boolean;
+    menuId?: string | null;
+    sharedBy?: string;
+    hovered?: boolean;
+  }) => void;
+  _removePopupAfterTimeout: (popup: PopupData) => void;
+  _updateExistingPopup: (popup: PopupData, newPopup: PopupData) => void;
+  /**
+   * React on detached menu (popup in VR) update and show a regular HTML popup.
+   */
+  onMenuDetached: ({
+    objectId,
+    userId,
+    detachId,
+  }: MenuDetachedForwardMessage) => void;
+  onRestorePopups: (popups: SerializedPopup[]) => void;
+  onMenuClosed: ({
+    originalMessage: { menuId },
+  }: ForwardedMessage<DetachedMenuClosedMessage>) => void;
+  /**
+   * Updates mesh reference of popup with given ID in popup data.
+   */
+  updateMeshReference: (popup: PopupData) => void;
+  willDestroy: () => void;
 }
 
 export const usePopupHandlerStore = create<PopupHandlerState>((set, get) => ({
@@ -132,17 +176,287 @@ export const usePopupHandlerStore = create<PopupHandlerState>((set, get) => ({
       isPinned: true,
     };
 
-    const newPopupData = [
-      ...get().popupData.filter((element) => {
-        element !== popup;
-      }),
-      newPopup,
-    ];
-
     set({
-      popupData: newPopupData,
+      popupData: [
+        ...get().popupData.filter((element) => {
+          element !== popup;
+        }),
+        newPopup,
+      ],
     });
   },
+
+  removePopup: async (entityId: string) => {
+    const popup = get().popupData.find((pd) => pd.entity.id === entityId);
+    if (!popup) {
+      return;
+    }
+
+    if (await canRemovePopup(popup)) {
+      set({
+        popupData: get().popupData.filter((pd) => pd.entity.id !== entityId),
+      });
+    } else {
+      useToastHandlerStore
+        .getState()
+        .showErrorToastMessage(
+          'Could not remove popup since it is currently in use by another user.'
+        );
+    }
+  },
+
+  handleMouseMove: (event: MouseEvent) => {
+    set({
+      latestMousePosition: {
+        timestamp: Date.now(),
+        x: event.pageX,
+        y: event.pageY,
+      },
+      isShiftPressed: event.shiftKey,
+    });
+  },
+
+  handleHoverOnMesh: (mesh?: THREE.Object3D) => {
+    if (isEntityMesh(mesh)) {
+      set({
+        popupData: get().popupData.map((pd) => ({
+          ...pd,
+          hovered: pd.entity.id === mesh.getModelId(),
+        })),
+      });
+    } else {
+      set({
+        popupData: get().popupData.map((pd) => ({ ...pd, hovered: false })),
+      });
+    }
+  },
+
+  addPopup: ({
+    mesh,
+    position,
+    wasMoved,
+    pinned,
+    menuId,
+    sharedBy,
+    hovered,
+  }) => {
+    if (!isEntityMesh(mesh) || getStoredSettings().hidePopupDelay.value == 0) {
+      return;
+    }
+    if (get().deactivated) return;
+
+    let popupPosition = position;
+
+    // Popups shared by other users have no position information
+    if (!popupPosition) {
+      popupPosition = {
+        x: 100,
+        y: 200 + get().popupData.length * 50, // Stack popups vertically
+      };
+    }
+
+    const newPopup = new PopupData({
+      mouseX: popupPosition.x,
+      mouseY: popupPosition.y,
+      wasMoved: wasMoved || false,
+      entity: mesh.dataModel,
+      mesh,
+      applicationId: (
+        mesh.parent as ApplicationObject3D | Landscape3D
+      ).getModelId(),
+      menuId: menuId || null,
+      isPinned: pinned || false,
+      sharedBy: sharedBy || '',
+      hovered: hovered || false,
+    });
+
+    // Check if popup for entity already exists and update it if so
+    const maybePopup = get().popupData.find(
+      (pd) => pd.entity.id === newPopup.entity.id
+    );
+    if (maybePopup) {
+      get()._updateExistingPopup(maybePopup, newPopup);
+      return;
+    }
+
+    // Ensure that there is at most one unpinned popup
+    const unpinnedPopupIndex = get().popupData.findIndex((pd) => !pd.isPinned);
+
+    if (unpinnedPopupIndex === -1 || newPopup.isPinned) {
+      set({ popupData: [...get().popupData, newPopup] });
+    } else {
+      const unpinnedPopup = get().popupData[unpinnedPopupIndex];
+      // Replace unpinned popup
+      const newPopupData = [...get().popupData];
+      newPopupData[unpinnedPopupIndex] = newPopup;
+
+      // Place new popup at same position of previously moved popup
+      if (unpinnedPopup.wasMoved) {
+        newPopup.mouseX = unpinnedPopup.mouseX;
+        newPopup.mouseY = unpinnedPopup.mouseY;
+        newPopup.wasMoved = true;
+      }
+      set({ popupData: newPopupData });
+    }
+
+    get()._removePopupAfterTimeout(newPopup);
+  },
+
+  _removePopupAfterTimeout: (popup: PopupData) => {
+    const latestMousePosition = get().latestMousePosition;
+    // Store popup position
+    const mouseX = popup.mouseX;
+    const mouseY = popup.mouseY;
+
+    setTimeout(() => {
+      const maybePopup = get().popupData.find(
+        (pd) => pd.entity.id === popup.entity.id
+      );
+
+      // Popup no longer available
+      if (!maybePopup || maybePopup.wasMoved || popup.isPinned) {
+        return;
+      }
+
+      // Do not remove popup when mouse stayed (recently) on target entity or shift is pressed
+      if (
+        get().isShiftPressed ||
+        (latestMousePosition.x == get().latestMousePosition.x &&
+          latestMousePosition.y == get().latestMousePosition.y)
+      ) {
+        get()._removePopupAfterTimeout(popup);
+        return;
+      }
+
+      // Popup did not move (was not updated)
+      if (maybePopup.mouseX == mouseX && maybePopup.mouseY == mouseY) {
+        get().removePopup(popup.entity.id);
+        return;
+      }
+
+      get()._removePopupAfterTimeout(popup);
+    }, getStoredSettings().hidePopupDelay.value * 1000);
+  },
+
+  _updateExistingPopup: (popup: PopupData, newPopup: PopupData) => {
+    if (get().deactivated) return;
+    set({
+      popupData: get().popupData.map((pd) => {
+        return pd.entity.id === popup.entity.id
+          ? {
+              ...popup,
+              wasMoved: popup.wasMoved || newPopup.wasMoved,
+              mouseX: newPopup.mouseX,
+              mouseY: newPopup.mouseY,
+              isPinned: popup.isPinned || newPopup.isPinned,
+              sharedBy: newPopup.sharedBy,
+            }
+          : pd;
+      }),
+    });
+    get().updateMeshReference(popup);
+  },
+
+  /**
+   * React on detached menu (popup in VR) update and show a regular HTML popup.
+   */
+  onMenuDetached: ({
+    objectId,
+    userId,
+    detachId,
+  }: MenuDetachedForwardMessage) => {
+    const mesh = useApplicationRendererStore.getState().getMeshById(detachId);
+    if (!mesh) {
+      return;
+    }
+    if (get().deactivated) return;
+
+    get().addPopup({
+      mesh,
+      wasMoved: true,
+      pinned: true,
+      sharedBy: userId,
+      menuId: objectId,
+    });
+  },
+
+  onRestorePopups: (popups: SerializedPopup[]) => {
+    if (get().deactivated) return;
+    set({ popupData: [] });
+
+    for (const popup of popups) {
+      const mesh = useApplicationRendererStore
+        .getState()
+        .getMeshById(popup.entityId);
+
+      if (!mesh) {
+        continue;
+      }
+
+      get().addPopup({
+        mesh,
+        wasMoved: true,
+        pinned: true,
+        sharedBy: popup.userId || undefined,
+        menuId: popup.menuId,
+      });
+    }
+  },
+
+  onMenuClosed: ({
+    originalMessage: { menuId },
+  }: ForwardedMessage<DetachedMenuClosedMessage>) => {
+    set({ popupData: get().popupData.filter((pd) => pd.menuId !== menuId) });
+  },
+
+  /**
+   * Updates mesh reference of popup with given ID in popup data.
+   */
+  updateMeshReference: (popup: PopupData) => {
+    const mesh = useApplicationRendererStore
+      .getState()
+      .getMeshById(popup.entity.id);
+    if (isEntityMesh(mesh)) {
+      set({
+        popupData: get().popupData.map((pd) => {
+          return pd.entity.id === popup.entity.id ? { ...pd, mesh: mesh } : pd;
+        }),
+      });
+    }
+  },
+
+  willDestroy: () => {
+    eventEmitter.off(MENU_DETACHED_EVENT, get().onMenuDetached);
+    eventEmitter.off(DETACHED_MENU_CLOSED_EVENT, get().onMenuClosed);
+    eventEmitter.off('restore_popups', get().onRestorePopups);
+  },
 }));
+
+async function canRemovePopup(popup: PopupData) {
+  // Popup / menu cannot be grabbed by other user without menuId
+  if (!popup.menuId) {
+    return true;
+  }
+
+  return useWebSocketStore
+    .getState()
+    .sendRespondableMessage<DetachedMenuClosedMessage, ObjectClosedResponse>(
+      DETACHED_MENU_CLOSED_EVENT,
+      {
+        event: 'detached_menu_closed',
+        menuId: popup.menuId,
+        nonce: 0, // will be overwritten
+      },
+      {
+        responseType: isObjectClosedResponse,
+        onResponse: (response: ObjectClosedResponse) => {
+          return response.isSuccess;
+        },
+        onOffline: () => {
+          return true;
+        },
+      }
+    );
+}
 
 usePopupHandlerStore.getState()._constructor();
