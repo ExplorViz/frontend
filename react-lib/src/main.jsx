dd71879ed758aa41f7edb588bf871d7f0831561e
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-03-10 10:13:33 +0100

    Configured Vite, added .env for Vite environment, added sass (support for scss)
    Former-commit-id: 512db1e1038dae74c5acc0998516d74d02fd1c03

--- react-lib/src/main.jsx
+++ react-lib/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Landscapes from './templates/landscapes.tsx';
+import Landscapes from './pages/landscapes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
