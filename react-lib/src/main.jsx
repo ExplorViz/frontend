7a436cd718070810212c1504ff4f6fd27c8b5ae7
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-03-10 11:26:45 +0100

    Changed React-Router conf
    Former-commit-id: 081dd6ec4fa9b46ea2a7dd11865b4e766abb9eb6

--- react-lib/src/main.jsx
+++ react-lib/src/main.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
 import Landscapes from './pages/landscapes.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Landscapes />} />
+      <Route path="/" element={<Navigate to="/landscapes" />} />
+      <Route path="/landscapes" element={<Landscapes />} />
+      {/* <Route path="/visualization" element={<Visualization />} /> */}
+      {/* <Route path="/settings" element={<Settings />} /> */}
+      <Route path="*" element={<Navigate to="/landscapes" />} />
     </Routes>
   </BrowserRouter>
 );
--- react-lib/src/pages/landscapes.tsx
+++ react-lib/src/pages/landscapes.tsx
@@ -2,5 +2,9 @@ import React, { ReactNode } from 'react';
 
 // TODO: Just as placeholder
 export default function Landscapes() {
-  return <div />;
+  return (
+    <div>
+      <p>Hello</p>
+    </div>
+  );
 }
