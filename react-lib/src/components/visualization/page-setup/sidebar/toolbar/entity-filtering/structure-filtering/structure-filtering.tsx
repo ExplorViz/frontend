b9b7bab7ef0fae27caeba4537865015364fc7d15
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-03-11 15:33:31 +0100

    Migrate component: structure-filtering
    Former-commit-id: e21afe1898865f39a081b17ef88741d0fa9fd790

---
+++ react-lib/src/components/visualization/page-setup/sidebar/toolbar/entity-filtering/structure-filtering/structure-filtering.tsx
@@ -0,0 +1,172 @@
+import React, { useEffect, useRef, useState } from 'react';
+
+import ClassMethodFiltering from 'react-lib/src/components/visualization/page-setup/sidebar/toolbar/entity-filtering/structure-filtering/class-method-filtering';
+import { DynamicLandscapeData } from 'react-lib/src/utils/landscape-schemes/dynamic/dynamic-data';
+import {
+  Class,
+  Package,
+  StructureLandscapeData,
+} from 'react-lib/src/utils/landscape-schemes/structure-data';
+import { getAllClassesInApplication } from 'react-lib/src/utils/application-helpers';
+import { NEW_SELECTED_TIMESTAMP_EVENT } from 'react-lib/src/stores/timestamp';
+import { LandscapeData } from 'react-lib/src/utils/landscape-schemes/landscape-data';
+import eventEmitter from 'react-lib/src/utils/event-emitter';
+
+interface StructureFilteringProps {
+  readonly landscapeData: LandscapeData;
+  triggerRenderingForGivenLandscapeData(
+    structureData: StructureLandscapeData,
+    dynamicData: DynamicLandscapeData
+  ): void;
+  pauseVisualizationUpdating(): void;
+}
+
+export default function StructureFiltering({
+  landscapeData,
+  triggerRenderingForGivenLandscapeData,
+  pauseVisualizationUpdating,
+}: StructureFilteringProps) {
+  const [classes, setClasses] = useState<Class[]>([]);
+  const [initialClasses, setInitialClasses] = useState<Class[]>([]);
+  const [
+    numRemainingClassesAfterFilteredByMethodCount,
+    setNumRemainingClassesAfterFilteredByMethodCount,
+  ] = useState<number>(0);
+  const initialLandscapeData = useRef<LandscapeData | undefined>(undefined);
+  const selectedMinMethodCount = useRef<number>(0);
+  const initialClassCount = initialClasses.length;
+
+  const classCount = (() => {
+    let classes: Class[] = [];
+
+    for (const node of landscapeData.structureLandscapeData.nodes) {
+      for (const app of node.applications) {
+        classes = [...classes, ...getAllClassesInApplication(app)];
+      }
+    }
+    setClasses(classes);
+    return classes.length;
+  })();
+
+  const updateMinMethodCount = (newMinMethodCount: number) => {
+    selectedMinMethodCount.current = newMinMethodCount;
+    _triggerRenderingForGivenLandscapeData();
+  };
+
+  const resetState = () => {
+    // reset state, since new timestamp has been loaded
+    let classes: Class[] = [];
+
+    for (const node of landscapeData.structureLandscapeData.nodes) {
+      for (const app of node.applications) {
+        classes = [...classes, ...getAllClassesInApplication(app)];
+      }
+    }
+    setInitialClasses(classes);
+    initialLandscapeData.current = landscapeData;
+    setNumRemainingClassesAfterFilteredByMethodCount(initialClasses.length);
+  };
+
+  const removeFilteredClassesOfNestedPackages = (
+    pack: Package,
+    classesToRemove: Class[]
+  ) => {
+    if (classesToRemove.length == 0) {
+      return;
+    }
+
+    for (let i = pack.classes.length - 1; i >= 0; i--) {
+      if (classesToRemove.find((clazz) => clazz.id == pack.classes[i].id)) {
+        pack.classes.splice(i, 1);
+      }
+    }
+
+    for (const subPack of pack.subPackages) {
+      removeFilteredClassesOfNestedPackages(subPack, classesToRemove);
+    }
+  };
+
+  const _triggerRenderingForGivenLandscapeData = () => {
+    let numFilter = 0;
+
+    // hide all classes that have a strict lower method count than selected
+    const classesToRemove = initialClasses.filter((t) => {
+      if (t.methods.length < selectedMinMethodCount.current!) {
+        return true;
+      }
+      numFilter++;
+      return false;
+    });
+
+    setNumRemainingClassesAfterFilteredByMethodCount(numFilter);
+    numFilter = 0;
+
+    const deepCopyStructure = structuredClone(
+      initialLandscapeData.current!.structureLandscapeData
+    );
+
+    let classes: Class[] = [];
+
+    for (const node of deepCopyStructure.nodes) {
+      for (const app of node.applications) {
+        classes = [...classes, ...getAllClassesInApplication(app)];
+      }
+    }
+
+    classes = [];
+
+    for (const node of deepCopyStructure.nodes) {
+      for (const app of node.applications) {
+        for (const pack of app.packages) {
+          removeFilteredClassesOfNestedPackages(pack, classesToRemove);
+        }
+      }
+    }
+
+    for (const node of deepCopyStructure.nodes) {
+      for (const app of node.applications) {
+        classes = [...classes, ...getAllClassesInApplication(app)];
+      }
+    }
+
+    classes = [];
+
+    triggerRenderingForGivenLandscapeData(
+      deepCopyStructure,
+      landscapeData.dynamicLandscapeData
+    );
+  };
+
+  useEffect(() => {
+    resetState();
+    eventEmitter.on(NEW_SELECTED_TIMESTAMP_EVENT, resetState);
+    return () => {
+      triggerRenderingForGivenLandscapeData(
+        initialLandscapeData.current!.structureLandscapeData,
+        initialLandscapeData.current!.dynamicLandscapeData
+      );
+      eventEmitter.off(NEW_SELECTED_TIMESTAMP_EVENT, resetState);
+    };
+  }, []);
+
+  return (
+    <>
+      <h6 className="mb-3 mt-3">
+        <strong>
+          Classes (# shown:
+          {classCount}/{initialClassCount})
+        </strong>
+      </h6>
+
+      <ClassMethodFiltering
+        classes={classes}
+        updateMinMethodCount={updateMinMethodCount}
+        pauseVisualizationUpdating={pauseVisualizationUpdating}
+        remainingEntityCountAfterFiltering={
+          numRemainingClassesAfterFilteredByMethodCount
+        }
+        initialEntityCount={initialClassCount}
+      />
+    </>
+  );
+}
