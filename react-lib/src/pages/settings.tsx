49a8310b1cac387f02340df67a7bb07d594696a7
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-03-11 17:44:58 +0100

    Added settings page
    Former-commit-id: 82fd6b1d0c4d8afad8fd1cfe126eef8ea46955ae

---
+++ react-lib/src/pages/settings.tsx
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import { useUserApiTokenStore, ApiToken } from '../stores/user-api-token';
+// import ApiTokenSelection from 'react-lib/src/components/api-token-selection';
+
+export default function Settings() {
+  const [refreshKey, setRefreshKey] = useState<number>(0);
+  const [apiTokens, setApiTokens] = useState<ApiToken[]>([]);
+
+  const retrieveApiTokens = useUserApiTokenStore(
+    (state) => state.retrieveApiTokens
+  );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const apiTokens = await retrieveApiTokens();
+
+      setApiTokens(apiTokens);
+    };
+
+    fetchData();
+  }, [refreshKey]);
+
+  const refreshRoute = () => {
+    setRefreshKey((prev) => prev++);
+  };
+
+  return (
+    <div
+      className={'container overflow-auto mt-5 mb-5 d-flex flex-column flex-1'}
+    >
+      <h4 className={'text-center mt-4 mb-3'}>Settings</h4>
+      <div className={'d-flex flex-row justify-content-center overflow-scroll'}>
+        {/* <ApiTokenSelection apiTokens={apiTokens} /> */}
+      </div>
+    </div>
+  );
+}
