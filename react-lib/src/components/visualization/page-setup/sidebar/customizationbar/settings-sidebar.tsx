26832463ce6f1aaf809e397c84fff3454d562b98
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-03-13 13:39:45 +0100

    Migrate component: settings-sidebar.tsx
    Former-commit-id: e9d53a87e524bd0af62d9bd552cb56871a9803c6

---
+++ react-lib/src/components/visualization/page-setup/sidebar/customizationbar/settings-sidebar.tsx
@@ -0,0 +1,37 @@
+import { XIcon } from '@primer/octicons-react';
+import React, { ReactNode } from 'react';
+import SidebarResizer from '../sidebar-resizer';
+import UndoRestructure from 'react-lib/src/components/visualization/page-setup/sidebar/customizationbar/restructure/undo-restructure';
+
+interface SettingsSidebarProps {
+  closeSettingsSidebar: () => void;
+  children: ReactNode[];
+}
+
+export default function SettingsSidebar({
+  closeSettingsSidebar,
+  children,
+}: SettingsSidebarProps) {
+  return (
+    <>
+      <div id="sidebarButtonContainer" className="foreground">
+        <button
+          type="button"
+          className="btn btn-light btn-outline-dark sidebar-button"
+          title="Close Sidebar"
+          aria-label="Close"
+          onClick={closeSettingsSidebar}
+        >
+          <XIcon size="small" className="align-middle" />
+        </button>
+        <SidebarResizer
+          buttonName="sidebarDragButton"
+          containerName="sidebarButtonContainer"
+          sidebarName="settingsSidebar"
+        />
+        <UndoRestructure />
+      </div>
+      <div className="sidebar-card-container pr-3 pl-3 pb-3">{children}</div>
+    </>
+  );
+}
