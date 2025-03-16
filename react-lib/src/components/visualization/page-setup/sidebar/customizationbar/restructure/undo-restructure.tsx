8edd483515dc9abd564dd1cc5094e8f2caaa2122
Moritz Bugla <stu237520@mail.uni-kiel.de>
2025-03-16 11:02:44 +0100

    Migrate component: undo-restructure
    Former-commit-id: e82b65f7440b7146fac1e666d703841ff85a1500

---
+++ react-lib/src/components/visualization/page-setup/sidebar/customizationbar/restructure/undo-restructure.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+import { useLandscapeRestructureStore } from 'react-lib/src/stores/landscape-restructure';
+import { useChangelogStore } from 'react-lib/src/stores/changelog';
+import { ReplyIcon } from '@primer/octicons-react';
+
+interface UndoRestructureProps {}
+
+export default function UndoRestructure({}: UndoRestructureProps) {
+  const canUndo = useLandscapeRestructureStore(
+    (state) => state.restructureMode
+  );
+  const undoBundledEntries = useLandscapeRestructureStore(
+    (state) => state.undoBundledEntries
+  );
+  const undoEntry = useLandscapeRestructureStore((state) => state.undoEntry);
+  const changeLogEntries = useChangelogStore((state) => state.changeLogEntries);
+  const isCreateBundle = useChangelogStore((state) => state.isCreateBundle);
+
+  const undoAction = () => {
+    if (!changeLogEntries.length) return;
+    const lastElementIndex = changeLogEntries.length - 1;
+    const lastElement = changeLogEntries[lastElementIndex];
+    const bundledCreateEntries = isCreateBundle(lastElement, [])?.reverse();
+
+    if (bundledCreateEntries?.length) {
+      undoBundledEntries(bundledCreateEntries);
+    } else {
+      undoEntry(lastElement);
+    }
+  };
+
+  return (
+    canUndo && (
+      <button
+        type="button"
+        className="btn btn-light btn-outline-dark sidebar-button"
+        title="Undo"
+        aria-label="Undo"
+        onClick={undoAction}
+        // disabled={changeLogEntries.length > 0}
+      >
+        <ReplyIcon size="small" className="align-middle" />
+      </button>
+    )
+  );
+}
