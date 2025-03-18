719b53799834526757808385792e74f803d89791
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-03-18 16:01:58 +0100

    Added auto-select-landscape component (now part of application component)
    Former-commit-id: da40655f1cfdbdb2bb720473ed5065d53afc13aa

--- react-lib/src/pages/application.tsx
+++ react-lib/src/pages/application.tsx
@@ -1,25 +1,102 @@
-import React, { useState } from 'react';
-import { useSearchParams, Outlet } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useSearchParams, Outlet, useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../stores/auth';
 import Navbar from 'react-lib/src/components/page-setup/navbar';
-// import AutoSelectLandscape from '../components/collaboration/auto-select-landscape';
 import ToastMessage from '../components/page-setup/toast-message';
 import { useInitNavigation } from 'react-lib/src/stores/store-router';
-
-// TODO: Uncomment AutoSelect after it exists
+import { useSnapshotTokenStore } from '../stores/snapshot-token';
+import {
+  LandscapeToken,
+  useLandscapeTokenStore,
+} from '../stores/landscape-token';
 
 export default function Application() {
   const [tokenId, setTokenId] = useState<string>('');
-  let [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
+
   const user = useAuthStore((state) => state.user);
+  const accessToken = useAuthStore((state) => state.accessToken);
+  const retrieveToken = useSnapshotTokenStore((state) => state.retrieveToken);
+  const setSnapshotToken = useSnapshotTokenStore((state) => state.setToken);
+  const landscapeToken = useLandscapeTokenStore((state) => state.token);
+  const setLandscapeToken = useLandscapeTokenStore((state) => state.setToken);
+
+  const navigate = useNavigate();
 
   useInitNavigation();
 
+  useEffect(() => {
+    const getLandscapeTokens = async () => {
+      const uId = user?.sub;
+
+      if (!uId) {
+        throw new Error('Could not find user');
+      }
+
+      const encodedUid = encodeURI(uId);
+      const response = await fetch(
+        `${import.meta.env.VITE_USER_SERV_URL}/user/${encodedUid}/token`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error('Bad response from user service');
+      }
+
+      return (await response.json()) as LandscapeToken[];
+    };
+
+    const autoSelectLandscape = async () => {
+      if (
+        searchParams.get('owner') &&
+        searchParams.get('createdAt') &&
+        searchParams.get('sharedSnapshot') !== undefined
+      ) {
+        const token = await retrieveToken(
+          searchParams.get('owner')!,
+          +searchParams.get('createdAt')!,
+          searchParams.get('sharedSnapshot') == 'true'
+        );
+
+        setSnapshotToken(token);
+        navigate('/visualization');
+      } else {
+        if (
+          !searchParams.get('landscapeToken') ||
+          landscapeToken?.value === searchParams.get('landscapeToken')
+        ) {
+          return;
+        }
+        try {
+          const tokens = await getLandscapeTokens();
+          const selectedToken = tokens.find(
+            (token) => token.value === searchParams.get('landscapeToken')
+          );
+
+          if (selectedToken) {
+            setLandscapeToken(selectedToken);
+            navigate('/visualization');
+          } else {
+            // Token not found => remove faulty query param
+            navigate('/landscapes');
+          }
+        } catch (error) {
+          console.error('Error in setUpLandscapeSelection:', error);
+        }
+      }
+    };
+
+    if (user) {
+      autoSelectLandscape();
+    }
+  }, []);
+
   return (
     <>
-      {/* <AutoSelectLandscape
-        landscapeToken={searchParams.get('landscapeToken')}
-      /> */}
       <div id="ember-right-click-menu-wormhole"></div>
       <div id="main-container">
         {user && (
