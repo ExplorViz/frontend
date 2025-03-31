872f11ac525925994761859ff930dacdd7f6788c
Philipp Wulff <stu234669@mail.uni-kiel.de>
2025-03-31 17:53:03 +0200

    Adjusted prettierrc and ci
    Former-commit-id: cba1b3b03c801653997e35d9acb7340dd29b6a51

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,7 +2,6 @@ const config = {
   singleQuote: true,
   trailingComma: 'es5',
   printWidth: 80,
-  plugins: ['prettier-plugin-ember-template-tag'],
   overrides: [
     {
       files: '*.{js,ts,jsx,tsx}',
