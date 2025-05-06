00a6a574a778c46e29bb754a6a8d00007091e874
Malte Hansen <malte.hanseat@gmail.com>
2025-05-06 17:28:06 +0200

    Revert vite config

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
-      "explorviz-frontend": path.resolve(__dirname),
-      "*": ["types/*"]
+      'explorviz-frontend': path.resolve(__dirname),
+      '*': ['types/*'],
     },
   },
   server: {
