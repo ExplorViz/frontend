e0048d43e67c7fee7c833c8f254fcb67070e8b12
Malte Hansen <malte.hanseat@gmail.com>
2025-05-19 10:42:56 +0200

    Generate random names for users

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -7,12 +7,67 @@ interface AuthState {
 
 export const useAuthStore = create<AuthState>((/*set, get*/) => ({
   user: {
-    name: import.meta.env.VITE_AUTH_DISABLED_PROFILE_NAME,
+    name: getRandomName(),
     nickname: import.meta.env.VITE_AUTH_DISABLED_NICKNAME,
     sub: import.meta.env.VITE_AUTH_DISABLED_SUB,
   },
   accessToken: import.meta.env.VITE_AUTH_DISABLED_ACCESS_TOKEN,
 }));
+
+function getRandomName() {
+  const NAMES = [
+    'Alex',
+    'Angel',
+    'Arden',
+    'Ari',
+    'Aspen',
+    'August',
+    'Avery',
+    'Baylor',
+    'Billie',
+    'Blair',
+    'Blake',
+    'Blake',
+    'Cameron',
+    'Casey',
+    'Charlie',
+    'Dakota',
+    'Devon',
+    'Drew',
+    'Ellis',
+    'Frankie',
+    'Grey',
+    'Harley',
+    'Indigo',
+    'Jamie',
+    'Jesse',
+    'Jordan',
+    'Justice',
+    'Kai',
+    'London',
+    'Micah',
+    'Morgan',
+    'Parker',
+    'Phoenix',
+    'Quinn',
+    'Reese',
+    'Remy',
+    'Riley',
+    'River',
+    'Rowan',
+    'Sage',
+    'Sam',
+    'Sasha',
+    'Skyler',
+    'Spencer',
+    'Sydney',
+    'Tatum',
+    'Taylor',
+    'Winter',
+  ];
+  const randomIndex = Math.floor(Math.random() * NAMES.length);
+  return NAMES[randomIndex];
+}
 
 export type AuthenticatedUser = {
   name: string;
