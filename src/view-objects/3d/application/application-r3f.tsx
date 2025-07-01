6ed29eba67dcb796527739d81b511972e52c04eb
Malte Hansen <malte.hanseat@gmail.com>
2025-07-01 18:58:37 +0200

    Refactor application r3f

--- src/view-objects/3d/application/application-r3f.tsx
+++ src/view-objects/3d/application/application-r3f.tsx
@@ -11,7 +11,7 @@ import ComponentR3F from 'explorviz-frontend/src/view-objects/3d/application/com
 import EmbeddedBrowser from 'explorviz-frontend/src/view-objects/3d/application/embedded-browser';
 import FoundationR3F from 'explorviz-frontend/src/view-objects/3d/application/foundation-r3f';
 import BoxLayout from 'explorviz-frontend/src/view-objects/layout-models/box-layout';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useState } from 'react';
 import { useShallow } from 'zustand/react/shallow';
 
 export default function ApplicationR3F({
@@ -22,18 +22,10 @@ export default function ApplicationR3F({
   layoutMap: Map<string, BoxLayout>;
 }) {
   const [isBrowserActive, setIsBrowserActive] = useState(false);
-  const [applicationPosition, setApplicationPosition] = useState<
-    THREE.Vector3 | undefined
-  >(layoutMap.get(applicationData.getId())?.position);
 
   const computeCommunicationLayout = useLinkRendererStore(
     (state) => state.computeCommunicationLayout
   );
-
-  useEffect(() => {
-    setApplicationPosition(layoutMap.get(applicationData.getId())?.position);
-  }, [applicationData, layoutMap]);
-
   const { isCommRendered } = useConfigurationStore(
     useShallow((state) => ({
       isCommRendered: state.isCommRendered,
@@ -41,82 +33,78 @@ export default function ApplicationR3F({
   );
 
   return (
-    <>
-      {applicationPosition && (
-        <group position={applicationPosition}>
-          <Root positionBottom={15} positionLeft={0} pixelSize={1}>
-            <Container>
-              <Button
-                width={25}
-                height={25}
-                padding={5}
-                backgroundColor={isBrowserActive ? 'red' : 'black'}
-                onClick={() => {
-                  setIsBrowserActive(!isBrowserActive);
-                }}
-              >
-                <AppWindow />
-              </Button>
-            </Container>
-          </Root>
-          {isBrowserActive && (
-            <EmbeddedBrowser application={applicationData.application} />
-          )}
-          {layoutMap.get(applicationData.getId()) && (
-            <FoundationR3F
-              application={applicationData.application}
-              layout={layoutMap.get(applicationData.getId())!}
-            />
-          )}
-
-          <Instances limit={5000} frustumCulled={false}>
-            <boxGeometry />
-            <meshLambertMaterial />
-            {applicationData
-              .getPackages()
-              .map((packageData) =>
-                layoutMap.get(packageData.id) ? (
-                  <ComponentR3F
-                    key={packageData.id}
-                    component={packageData}
-                    layout={layoutMap.get(packageData.id)!}
-                  />
-                ) : (
-                  <Fragment key={packageData.id} />
-                )
-              )}
-          </Instances>
-          <Instances limit={50000} frustumCulled={false}>
-            <boxGeometry />
-            <meshLambertMaterial />
-            {applicationData
-              .getClasses()
-              .map((classData) =>
-                layoutMap.get(classData.id) ? (
-                  <ClassR3F
-                    key={classData.id}
-                    dataModel={classData}
-                    layout={layoutMap.get(classData.id)!}
-                  />
-                ) : (
-                  <Fragment key={classData.id} />
-                )
-              )}
-          </Instances>
-          {isCommRendered &&
-            applicationData.classCommunications.map((communication) => (
-              <CommunicationR3F
-                key={communication.id}
-                communicationModel={communication}
-                communicationLayout={computeCommunicationLayout(
-                  communication,
-                  [applicationData],
-                  layoutMap
-                )}
-              />
-            ))}
-        </group>
+    <group position={layoutMap.get(applicationData.getId())?.position}>
+      <Root positionBottom={15} positionLeft={0} pixelSize={1}>
+        <Container>
+          <Button
+            width={25}
+            height={25}
+            padding={5}
+            backgroundColor={isBrowserActive ? 'red' : 'black'}
+            onClick={() => {
+              setIsBrowserActive(!isBrowserActive);
+            }}
+          >
+            <AppWindow />
+          </Button>
+        </Container>
+      </Root>
+      {isBrowserActive && (
+        <EmbeddedBrowser application={applicationData.application} />
       )}
-    </>
+      {layoutMap.get(applicationData.getId()) && (
+        <FoundationR3F
+          application={applicationData.application}
+          layout={layoutMap.get(applicationData.getId())!}
+        />
+      )}
+
+      <Instances limit={5000} frustumCulled={false}>
+        <boxGeometry />
+        <meshLambertMaterial />
+        {applicationData
+          .getPackages()
+          .map((packageData) =>
+            layoutMap.get(packageData.id) ? (
+              <ComponentR3F
+                key={packageData.id}
+                component={packageData}
+                layout={layoutMap.get(packageData.id)!}
+              />
+            ) : (
+              <Fragment key={packageData.id} />
+            )
+          )}
+      </Instances>
+      <Instances limit={50000} frustumCulled={false}>
+        <boxGeometry />
+        <meshLambertMaterial />
+        {applicationData
+          .getClasses()
+          .map((classData) =>
+            layoutMap.get(classData.id) ? (
+              <ClassR3F
+                key={classData.id}
+                dataModel={classData}
+                layout={layoutMap.get(classData.id)!}
+              />
+            ) : (
+              <Fragment key={classData.id} />
+            )
+          )}
+      </Instances>
+      {isCommRendered &&
+        applicationData.classCommunications.map((communication) => (
+          <CommunicationR3F
+            key={communication.id}
+            communicationModel={communication}
+            communicationLayout={computeCommunicationLayout(
+              communication,
+              [applicationData],
+              layoutMap
+            )}
+          />
+        ))}
+    </group>
   );
 }
