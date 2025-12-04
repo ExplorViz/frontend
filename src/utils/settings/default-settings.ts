8242c7c99859a7a2c868bcf6c05d36914eefdae8
Malte Hansen <malte.hanseat@gmail.com>
2025-12-04 13:45:44 +0100

    Allow 0.01 step for edge bundling beta

--- src/utils/settings/default-settings.ts
+++ src/utils/settings/default-settings.ts
@@ -476,7 +476,7 @@ export const defaultVizSettings: VisualizationSettings = {
   beta: {
     level: SettingLevel.DEFAULT,
     value: 0.8,
-    range: { min: 0.0, max: 1.0, step: 0.1 },
+    range: { min: 0.0, max: 1.0, step: 0.01 },
     group: 'Communication',
     displayName: '3D-HAP Attraction Power (β)',
     description:
