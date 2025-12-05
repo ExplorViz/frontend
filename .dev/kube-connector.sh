9248af9ce20dc8c1bea299ca3a34e2f7feca731b
Peer Broxtermann <broxtermann.peer@gmail.com>
2025-12-05 15:12:18 +0100

    Server-based Graphviz, simple loading of SVGs in Toolbar

--- .dev/kube-connector.sh
+++ .dev/kube-connector.sh
@@ -1,26 +1,18 @@
 #!/bin/sh
 
-
 SCRIPT_DIR=$(dirname "$0")
 PROJECT_ROOT="$SCRIPT_DIR/.."
 
 INPUT_DIR="$PROJECT_ROOT/sample-manifests"
-OUTPUT_DIR="$PROJECT_ROOT/src/components/manifestDOT"
+OUTPUT_DIR="$PROJECT_ROOT/public/manifest-svgs"
 
 mkdir -p "$OUTPUT_DIR"
 
-echo "📦 Generating .dot files from Kubernetes manifests..."
-
 for file in "$INPUT_DIR"/*.y*ml; do
-    [ -e "$file" ] || continue     # skip if no files found
+    [ -e "$file" ] || continue
 
     filename=$(basename "$file")
     name="${filename%.*}"
 
-    echo "➡️  Processing $file -> $OUTPUT_DIR/$name.dot"
-
-    kube-diagrams -o "$OUTPUT_DIR/$name.dot" "$file"
+    kube-diagrams --embed-all-icons -o "$OUTPUT_DIR/$name.svg" "$file"
 done
-
-echo "✅ Done!"
-
--- src/components/visualization/page-setup/sidebar/toolbar/kubernetes-diagrams/kubernetes-diagrams.tsx
+++ src/components/visualization/page-setup/sidebar/toolbar/kubernetes-diagrams/kubernetes-diagrams.tsx
@@ -1,1 +1,19 @@
-TODO
+import { useEffect, useState } from "react";
+
+const svgModules = import.meta.glob('/public/manifest-svgs/*.svg', { eager: true, import: 'default' });
+
+export default function KubernetesDiagrams() {
+  const [svgs, setSvgs] = useState<string[]>([]);
+
+  useEffect(() => {
+    setSvgs(Object.values(svgModules) as string[]);
+  }, []);
+
+  return (
+    <div className="w-full h-full p-4 flex flex-wrap gap-4">
+      {svgs.map((url, index) => (
+        <object key={index} type="image/svg+xml" data={url} className="w-24 h-24" />
+      ))}
+    </div>
+  );
+}
