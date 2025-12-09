93afdfc8fefbad6f84042d8e452e976a6c46e9b4
Malte Hansen <malte.hanseat@gmail.com>
2025-12-09 16:53:25 +0100

    Add nginx config

--- variable-substitute.sh
+++ variable-substitute.sh
@@ -13,6 +13,8 @@ find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed
 
 find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|change-span-url|$FRONTEND_HOST_URL|g" '{}' +
 
+find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|change-copilot-url|$FRONTEND_HOST_URL|g" '{}' +
+
 DEFAULT_NICKNAME="${VITE_AUTH_DISABLED_NICKNAME:-Jessy}"
 find /usr/share/nginx/html -type f \( -name '*.js' -o -name '*.css' \) -exec sed -i "s|change-nickname|$FRONTEND_HOST_URL|g" '{}' +
 sed -i "s#change-nickname#$DEFAULT_NICKNAME#g" /usr/share/nginx/html/index.html
