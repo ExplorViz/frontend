93d47f9810a7dcde0a85aacac84b7e295788e293
Malte Hansen <malte.hanseat@gmail.com>
2026-01-16 10:12:33 +0100

    Fix syntax in subsettings-menu

--- src/utils/extended-reality/vr-menus-r3f/subsettings-menu.tsx
+++ src/utils/extended-reality/vr-menus-r3f/subsettings-menu.tsx
@@ -1,18 +1,25 @@
 import { useState } from 'react';
-import { Container, Text } from '@react-three/uikit';
-import { Label, Switch, Slider, Button, RadioGroup, RadioGroupItem } from '@react-three/uikit-default';
 
-import { useUserSettingsStore } from "explorviz-frontend/src/stores/user-settings";
-import { 
-  isButtonSetting, 
-  isColorSetting, 
-  isFlagSetting, 
+import { Container, Text } from '@react-three/uikit';
+import {
+  Button,
+  Label,
+  RadioGroup,
+  RadioGroupItem,
+  Slider,
+  Switch,
+} from '@react-three/uikit-default';
+import { useUserSettingsStore } from 'explorviz-frontend/src/stores/user-settings';
+import {
+  isButtonSetting,
+  isColorSetting,
+  isFlagSetting,
   isRangeSetting,
   isSelectSetting,
   VisualizationSettingId,
-} from "../../settings/settings-schemas";
+} from '../../settings/settings-schemas';
 
-export default function SubsettingsMenu({ 
+export default function SubsettingsMenu({
   settingId,
 }: {
   settingId: VisualizationSettingId;
@@ -23,8 +30,10 @@ export default function SubsettingsMenu({
   const updateUserSetting = useUserSettingsStore(
     (state) => state.updateSetting
   );
-  const [sliderValue, setSliderValue] = useState<number>(isRangeSetting(setting) ? setting.value : 0);
-  
+  const [sliderValue, setSliderValue] = useState<number>(
+    isRangeSetting(setting) ? setting.value : 0
+  );
+
   const updateFlagSetting = (value?: boolean) => {
     try {
       updateUserSetting(settingId, value);
@@ -56,83 +65,68 @@ export default function SubsettingsMenu({
   };
 
   return (
-    {isFlagSetting(setting) && (
-      <Container 
-        flexDirection="row"
-        alignItems="center" 
-        gap={8} 
-      >
-        <Switch 
-          defaultChecked={!setting.value} 
-          onClick={() => updateFlagSetting(!setting.value)}
-        />
-        <Label>
-          <Text>{setting.displayName}</Text>
-        </Label>
-      </Container>
-    )}
-    {isRangeSetting(setting) && (
-      <Container 
-        flexDirection="row"
-        alignItems="center" 
-        gap={8} 
-      >
-        <Slider 
-          min={setting.range.min}
-          max={setting.range.max}
-          step={setting.range.step}
-          width={300}
-          value={sliderValue}
-          onValueChange={(v) =>
-            setSliderValue(v)
-            updateRangeSetting(v)
-          }
-        />
-        <Label>
-          <Text>{setting.displayName}</Text>
-        </Label>
-      </Container>
-    )}
-    {isColorSetting(setting) && (
-      <Text>Text</Text>
-    )}
-    {isButtonSetting(setting) && (
-      <Container 
-        flexDirection="row"
-        alignItems="center" 
-        gap={8} 
-      >
-        <Button size="icon">
-          <Text>{setting.buttonText}</Text>
-        </Button>
-        <Label>
-          <Text>{setting.displayName}</Text>
-        </Label>
-      </Container>
-    )}
-    {isSelectSetting(setting) && (
-      <Container
-        flexDirection="row"
-        alignItems="center"
-        gap={8}
-      >
-        <RadioGroup>
-          setting.options.map((option) => {
-            return (
-              <RadioGroupItem 
-                onClick={() => updateSelectSetting(option)}
+    <>
+      {isFlagSetting(setting) && (
+        <Container flexDirection="row" alignItems="center" gap={8}>
+          <Switch
+            defaultChecked={!setting.value}
+            onClick={() => updateFlagSetting(!setting.value)}
+          />
+          <Label>
+            <Text>{setting.displayName}</Text>
+          </Label>
+        </Container>
+      )}
+      ;
+      {isRangeSetting(setting) && (
+        <Container flexDirection="row" alignItems="center" gap={8}>
+          <Slider
+            min={setting.range.min}
+            max={setting.range.max}
+            step={setting.range.step}
+            width={300}
+            value={sliderValue}
+            onValueChange={(v) => {
+              setSliderValue(v);
+              updateRangeSetting(v);
+            }}
+          />
+          <Label>
+            <Text>{setting.displayName}</Text>
+          </Label>
+        </Container>
+      )}
+      {isColorSetting(setting) && <Text>Text</Text>}
+      {isButtonSetting(setting) && (
+        <Container flexDirection="row" alignItems="center" gap={8}>
+          <Button size="icon">
+            <Text>{setting.buttonText}</Text>
+          </Button>
+          <Label>
+            <Text>{setting.displayName}</Text>
+          </Label>
+        </Container>
+      )}
+      {isSelectSetting(setting) && (
+        <Container flexDirection="row" alignItems="center" gap={8}>
+          <RadioGroup>
+            {setting.options.map((option) => (
+              <RadioGroupItem
+                onClick={() => {
+                  updateSelectSetting(option);
+                }}
               >
                 <Label>
                   <Text>{option}</Text>
                 </Label>
               </RadioGroupItem>
-            );
-          })
-        </RadioGroup>
-        <Label>
-          <Text>{setting.displayName}</Text>
-        </Label>
-      </Container>
-    )}
+            ))}
+          </RadioGroup>
+          <Label>
+            <Text>{setting.displayName}</Text>
+          </Label>
+        </Container>
+      )}
+    </>
   );
 }
