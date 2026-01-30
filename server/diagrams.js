339101e39e258ba901975deda1be65058312b4f5
Peer Broxtermann <stu242486@mail.uni-kiel.de>
2026-01-30 16:18:17 +0100

    Single Icon Path for Kubernetes Icons, Error-Fix in diagrams.js

--- server/diagrams.js
+++ server/diagrams.js
@@ -10,12 +10,18 @@ export const diagramsRouter = express.Router();
 function run(cmd, args) {
   return new Promise((resolve, reject) => {
     const p = spawn(cmd, args);
+
+    let stdout = '';
     let stderr = '';
 
+    p.stdout.on('data', (d) => (stdout += d.toString()));
     p.stderr.on('data', (d) => (stderr += d.toString()));
-    p.on('close', (code) =>
-      code === 0 ? resolve() : reject(new Error(stderr))
-    );
+
+    p.on('error', (err) => reject(err));
+    p.on('close', (code) => {
+      if (code === 0) resolve(stdout);
+      else reject(new Error(stderr || stdout));
+    });
   });
 }
 
@@ -30,7 +36,6 @@ async function runShellAndStream(shellCmd, res) {
   const output = tmpSvgPath();
 
   await run('sh', ['-c', `${shellCmd} -o ${output}`]);
-
   streamSvgFile(output, res);
 }
 
@@ -38,25 +43,25 @@ function tmpSvgPath() {
   return path.join(os.tmpdir(), `kubediagram-${crypto.randomUUID()}.svg`);
 }
 
-function streamSvgFile(output, res) {
+async function streamSvgFile(output, res) {
   res.setHeader('Content-Type', 'image/svg+xml');
 
+  // wait until file exists and is non-empty
+  await new Promise((resolve, reject) => {
+    const check = () => {
+      fs.stat(output, (err, stats) => {
+        if (err) return reject(err);
+        if (stats.size > 0) resolve();
+        else setTimeout(check, 10);
+      });
+    };
+    check();
+  });
+
   const stream = fs.createReadStream(output, 'utf8');
+  stream.pipe(res);
 
-  stream.on('data', (chunk) => {
-    res.write(
-      chunk.replace(
-        /https:\/\/raw\.githubusercontent\.com\/mingrammer\/diagrams\/refs\/heads\/master\/resources/g,
-        '/kubeDiagrams-icons'
-      )
-    );
-  });
-
-  stream.on('end', () => {
-    res.end();
-    fs.unlink(output, () => {});
-  });
-
+  stream.on('close', () => fs.unlink(output, () => {}));
   stream.on('error', (err) => {
     console.error(err);
     res.destroy();
@@ -80,6 +85,8 @@ diagramsRouter.post('/', async (req, res) => {
         );
         break;
 
+      // TODO: it is also possible to generate a Diagram based on a Helm Chart using helm-diagrams
+      // https://github.com/philippemerle/KubeDiagrams/tree/main?tab=readme-ov-file#helm-diagrams
       case 'helmfile':
         await runShellAndStream(
           `helmfile template -f ${inputPath} | kube-diagrams -`,
