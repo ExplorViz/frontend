291d8cc4a9ae16ce2b179ff37bedd004ed998b5b
Moritz Bugla <stu237520@mail.uni-kiel.de>
2026-09-16 02:13:21 +0200

    Communication popup: Only show functions tab if code buildings are involved in communication

--- src/components/visualization/rendering/popups/city-popups/communication/communication-popup.tsx
+++ src/components/visualization/rendering/popups/city-popups/communication/communication-popup.tsx
@@ -1,5 +1,7 @@
 import PopupData from 'explorviz-frontend/src/components/visualization/rendering/popups/popup-data';
+import { useModelStore } from 'explorviz-frontend/src/stores/repos/model-repository';
 import AggregatedCommunication from 'explorviz-frontend/src/utils/landscape-schemes/dynamic/aggregated-communication';
+import { ModelType } from 'explorviz-frontend/src/utils/landscape-schemes/flat-landscape';
 import { useState } from 'react';
 import { Tab, Tabs } from 'react-bootstrap';
 import CommunicationSpansTab from './communication-spans-tab';
@@ -14,6 +16,29 @@ export default function CommunicationPopup({
   popupData,
 }: CommunicationPopupProps) {
   const communication = popupData.entity as AggregatedCommunication;
+  const buildingComms = communication.getBuildingCommunications();
+
+  const buildings = useModelStore((state) => state.buildings);
+  const telemetryKeyToEntityId = useModelStore(
+    (state) => state.telemetryKeyToEntityId
+  );
+
+  const buildingTypes = new Set<ModelType>();
+  for (const c of buildingComms) {
+    const targetId = telemetryKeyToEntityId.get(c.targetEntityKey);
+    const target = targetId ? buildings[targetId] : undefined;
+    if (target) {
+      buildingTypes.add(target.type);
+    }
+
+    if (c.isBidirectional) {
+      const sourceId = telemetryKeyToEntityId.get(c.sourceEntityKey);
+      const source = sourceId ? buildings[sourceId] : undefined;
+      if (source) {
+        buildingTypes.add(source.type);
+      }
+    }
+  }
 
   const [activeTab, setActiveTab] = useState<string>('general');
 
@@ -37,9 +62,11 @@ export default function CommunicationPopup({
           <Tab eventKey="spans" title="Spans" mountOnEnter={true}>
             <CommunicationSpansTab communication={communication} />
           </Tab>
-          <Tab eventKey="functions" title="Functions" mountOnEnter={true}>
-            <FunctionsTab communication={communication} />
-          </Tab>
+          {buildingTypes.has('code') && (
+            <Tab eventKey="functions" title="Functions" mountOnEnter={true}>
+              <FunctionsTab communication={communication} />
+            </Tab>
+          )}
         </Tabs>
       </div>
     </>
